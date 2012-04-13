9d392e31c071593798fcfd01d100f7346f75cfc0
Niko Ehrenfeuchter <mail@he1ix.org>
2012-04-13 11:33:58 +0200

    remove sandbox code from plugin

--- save_all_open_windows.py
+++ save_all_open_windows.py
@@ -31,36 +31,3 @@ for i in range(wcount):
 		print "imageID", imgid, "saved as", filename
 	else:
 		print "ERROR saving imageID", imgid, "file at", filepath
-
-#print "ID list of windows:", WindowManager.getIDList()
-##idlist = WindowManager.getIDList()
-##for wid in idlist:
-##	print "window id:", wid
-##
-##print "front window:", WindowManager.getFrontWindow()
-
-
-# wm.putBehind()
-
-#print "width:", imp.width
-#print "height:", imp.height
-#print "number of pixels:", imp.width * imp.height
-# print "# of slices:", imp.getNSlices(), ", cur:", imp.getSlice()
-# print "stack size:", imp.getImageStackSize(), ", channels:", imp.getNChannels()
-# framepos = imp.getT()
-# print "# of time frames:", imp.getNFrames(), ", cur:", framepos
-
-
-#print "switching to next hyperstack frame position..."
-#imp.setT(framepos + 1)
-#print "current hyperstack frame position:", imp.getT()
-
-#print "getOpenAsHyperStack:", imp.getOpenAsHyperStack()
-
-#types = {ImagePlus.COLOR_RGB : "RGB",
-#         ImagePlus.GRAY8 : "8-bit",
-#         ImagePlus.GRAY16 : "16-bit",
-#         ImagePlus.GRAY32 : "32-bit",
-#         ImagePlus.COLOR_256 : "8-bit color"}
-
-#print "image type:", imp.type, types[imp.type]
