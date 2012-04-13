96c4c97548b4c8cf944ad555e4b9da837d239243
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2012-04-13 15:09:01 +0200

    wrap code in a function

--- save_all_open_windows.py
+++ save_all_open_windows.py
@@ -2,32 +2,35 @@ from ij import IJ
 from ij.io import FileSaver
 from os import path
 
-target = 'C:/Users/ehrenfeu/Desktop/tmpfast/ivana/stitch_test'
-print "Using", target, "as destination folder"
+def run():
+	target = 'C:/Users/ehrenfeu/Desktop/tmpfast/ivana/stitch_test'
+	print "Using", target, "as destination folder"
 
-wm = WindowManager
+	wm = WindowManager
 
-wcount = wm.getWindowCount()
-print "number of windows:", wcount
+	wcount = wm.getWindowCount()
+	print "number of windows:", wcount
 
-# determine padding width
-pad = len(str(wcount))
+	# determine padding width
+	pad = len(str(wcount))
 
-for i in range(wcount):
-	### image ID lists start with 1, not 0
-	## wid = id + 1
-	imgid = wm.getNthImageID(i+1)
-	# print "window id ", i+1, ":", wm.getNthImageID(i+1)
-	imp = wm.getImage(i+1)
-	fs = FileSaver(imp)
-	# Print image details
-	# print "  title:", imp.title
-	# print "  current window:", wm.getCurrentWindow()
-	# Construct filename
-	filename = 'tile_' + str(i+1).zfill(pad) + '.tif'
-	filepath = target + '/' + filename
-	#print "filename:", filename
-	if fs.saveAsTiffStack(filepath):
-		print "imageID", imgid, "saved as", filename
-	else:
-		print "ERROR saving imageID", imgid, "file at", filepath
+	for i in range(wcount):
+		### image ID lists start with 1, not 0
+		## wid = id + 1
+		imgid = wm.getNthImageID(i+1)
+		# print "window id ", i+1, ":", wm.getNthImageID(i+1)
+		imp = wm.getImage(i+1)
+		fs = FileSaver(imp)
+		# Print image details
+		# print "  title:", imp.title
+		# print "  current window:", wm.getCurrentWindow()
+		# Construct filename
+		filename = 'tile_' + str(i+1).zfill(pad) + '.tif'
+		filepath = target + '/' + filename
+		#print "filename:", filename
+		if fs.saveAsTiffStack(filepath):
+			print "imageID", imgid, "saved as", filename
+		else:
+			print "ERROR saving imageID", imgid, "file at", filepath
+
+run()
