e7b26f1642f6ebbc2f00685efe0f36b5e1631673
Niko Ehrenfeuchter <mail@he1ix.org>
2012-08-08 16:41:40 +0200

    remove unrelated functions, move docstring

--- imaris/statistics/imaris_xml_parser.py
+++ imaris/statistics/imaris_xml_parser.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python
 
-"""Parse Excel XML files into a python datastructure.
-"""
-
 # TODO:
 #  - create a class for handling Excel XML, move to separate package
 #  - do sanity checking
@@ -14,17 +11,8 @@ import sys
 import math
 import numpy as np
 
-def calc_dist_xyz(p1, p2):
-	dx = abs(p2[0] - p1[0])
-	dy = abs(p2[1] - p1[1])
-	dz = abs(p2[2] - p1[2])
-	dist = math.sqrt(dx**2 + dy**2 + dz**2)
-	return(dist)
-
-def dist(p1, p2):
-	point1 = np.array(p1)
-	point2 = np.array(p2)
-	return(np.linalg.norm(point1 - point2))
+"""Parse Excel XML files into a python datastructure.
+"""
 
 def parse_xml(infile):
     print "Processing file: " + infile
