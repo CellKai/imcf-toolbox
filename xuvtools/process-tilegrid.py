66273d71b0c61361c9e3516d964a8bfbcb462141
Niko Ehrenfeuchter <mail@he1ix.org>
2012-09-12 14:27:48 +0200

    parse CSV file and convert into 2D numerical list

--- xuvtools/process-tilegrid.py
+++ xuvtools/process-tilegrid.py
@@ -4,22 +4,33 @@
 """
 
 # TODO:
-#  - use pyhton's csv module
 #  - parse tile size (pixels and real units) from the .xuv file
 #  - parse tile position information from CSV file
+#  - check if number of tiles from CSV matches .xuv file
 #  - generate new position information
 #  - update corresponding lines in .xuv file
 
-file = open('tiles-arrangement.csv')
-for line in file:
-	for num in line.rsplit(';'):
-		if num.isdigit():
-			print int(num)
+# file = open('tiles-arrangement.csv')
+# for line in file:
+# 	for num in line.rsplit(';'):
+# 		if num.isdigit():
+# 			print int(num)
 
 
-# >>> import csv
-# >>> spamReader = csv.reader(open('eggs.csv', 'rb'), delimiter=' ', quotechar='|')
-# >>> for row in spamReader:
-# ...     print ', '.join(row)
-# Spam, Spam, Spam, Spam, Spam, Baked Beans
-# Spam, Lovely Spam, Wonderful Spam
+import csv
+
+parsedata = csv.reader(open('tiles-arrangement.csv'), delimiter=';')
+data = [] # the 2D-list holding our tile numbers
+
+# parse elements of the row and discard all non-numerical ones:
+for row in parsedata:
+    row_num = [] # holds the converted numerical values
+    for num in row:
+        if num.isdigit():
+            row_num.append(int(num))
+        else:
+            row_num.append(None)
+    # the last entry holds the maxval of this line, we discard it:
+    data.append(row_num[0:-1])
+
+print data
