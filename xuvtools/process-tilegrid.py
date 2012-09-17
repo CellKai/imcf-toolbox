aaf075b7c01d75cc1bb53596be9da458fdb6afdb
Niko Ehrenfeuchter <mail@he1ix.org>
2012-09-17 22:02:09 +0200

    use argparse module to provide cmdline parameters

--- xuvtools/process-tilegrid.py
+++ xuvtools/process-tilegrid.py
@@ -9,17 +9,35 @@ this information.
 
 # TODO:
 #  - check if number of tiles from CSV matches .xuv file
-#  - use commandline parameters for input, output and overlap
 
 import csv
+import sys
+import argparse
 
-overlap = 0.15 # set overlap to 15 percent
+argparser = argparse.ArgumentParser(description=__doc__)
+argparser.add_argument('-p', '--overlap', type=float, default='0.15',
+    help='tile overlap (default 0.15)')
+argparser.add_argument('-t', '--tiles', required=True, type=file,
+    help='CSV file containing tile positions')
+argparser.add_argument('-i', '--infile', required=True, type=file,
+    help='Xuv input project file')
+argparser.add_argument('-o', '--outfile', type=argparse.FileType('w', 0),
+    help='Xuv input project file', required=True)
+try:
+    args = argparser.parse_args()
+except IOError as e:
+    argparser.error(str(e))
+
+# print args.__contains__
+# print type(args)
+# print args.infile
+# print args.outfile
 
 data = [] # the 2D-list holding our tile numbers
 tilemax = 0
 
 # parse elements of the row and discard all non-numerical ones:
-parsedata = csv.reader(open('tiles-arrangement.csv'), delimiter=';')
+parsedata = csv.reader(args.tiles, delimiter=';')
 for row in parsedata:
     row_num = [] # holds the converted numerical values
     for num in row:
@@ -50,8 +68,7 @@ size_um = []
 size_px = []
 xuvdata_orig = []
 
-xuvfile = open('file.xuv')
-for line in xuvfile:
+for line in args.infile:
     # remember original xuv file content
     xuvdata_orig.append(line)
     # strip trailing whitespaces and split key-value pairs
@@ -62,14 +79,13 @@ for line in xuvfile:
     if line_elt[0] == 'stack0001_size_pix':
         for size in line_elt[1].rsplit(','):
             size_px.append(int(size))
-xuvfile.close()
+args.infile.close()
 
 print size_um
 print size_px
 
 
 # generate new xuv file content
-xuvfile = open('newfile.xuv', 'w')
 for line in xuvdata_orig:
     line_elt = line.rstrip().rsplit('=')
     # scan for lines holding tile coordinates
@@ -77,12 +93,12 @@ for line in xuvdata_orig:
         prefix = line_elt[0].split('_', 1)[0]
         tileno = int(prefix[4:8])
         ## now calculate the new tile position
-        coord_y = tilepos[tileno - 1][0] * (1 - overlap)
-        coord_x = tilepos[tileno - 1][1] * (1 - overlap)
+        coord_y = tilepos[tileno - 1][0] * (1 - args.overlap)
+        coord_x = tilepos[tileno - 1][1] * (1 - args.overlap)
         coord_y = coord_y * size_px[1] * size_um[1]
         coord_x = coord_x * size_px[2] * size_um[2]
-        xuvfile.write(line_elt[0] + '=0,'
+        args.outfile.write(line_elt[0] + '=0,'
             + str(coord_y) + ',' + str(coord_x) + '\n')
     else:
-        xuvfile.write(line)
-xuvfile.close()
+        args.outfile.write(line)
+args.outfile.close()
