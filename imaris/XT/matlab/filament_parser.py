00e03306cb70894fb93cb8435b09ed12c646f1ea
Niko Ehrenfeuchter <mail@he1ix.org>
2012-09-20 00:36:02 +0200

    adjust docstring + argparser

--- imaris/XT/matlab/filament_parser.py
+++ imaris/XT/matlab/filament_parser.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python
 
-"""Parse a CSV file containing tile positions and update a Xuv projectfile with
-this information.
+"""Parse a CSV file containing filament point coordinates extracted from
+Imaris via the XT/Matlab interface.
 """
 
-# WARNING: this script assumes *ALL* stacks in the project file do
-# have the same size (x-y-z)!
-
 # TODO:
-#  - check if number of tiles from CSV matches .xuv file
+#  - parse CSV
+#  - build data structure from coordinates
 
 import csv
-import sys
 import argparse
 
 argparser = argparse.ArgumentParser(description=__doc__)
-argparser.add_argument('-p', '--overlap', type=float, default='0.15',
-    help='tile overlap (default 0.15)')
-argparser.add_argument('-t', '--tiles', required=True, type=file,
-    help='CSV file containing tile positions')
 argparser.add_argument('-i', '--infile', required=True, type=file,
-    help='Xuv input project file')
-argparser.add_argument('-o', '--outfile', type=argparse.FileType('w', 0),
-    help='Xuv input project file', required=True)
+    help='CSV file containing filament coordinates')
 try:
     args = argparser.parse_args()
 except IOError as e:
@@ -33,11 +24,14 @@ except IOError as e:
 # print args.infile
 # print args.outfile
 
+
+# FIXME: check code below for re-using it
+
 data = [] # the 2D-list holding our tile numbers
 tilemax = 0
 
 # parse elements of the row and discard all non-numerical ones:
-parsedata = csv.reader(args.tiles, delimiter=';')
+parsedata = csv.reader(args.infile, delimiter=',')
 for row in parsedata:
     row_num = [] # holds the converted numerical values
     for num in row:
@@ -62,43 +56,3 @@ for coord_y, line in enumerate(data):
             tilepos[tile - 1] = (coord_y, coord_x)
 print tilepos
 
-
-# parse xuv project file, get tile size etc.
-size_um = []
-size_px = []
-xuvdata_orig = []
-
-for line in args.infile:
-    # remember original xuv file content
-    xuvdata_orig.append(line)
-    # strip trailing whitespaces and split key-value pairs
-    line_elt = line.rstrip().rsplit('=')
-    if line_elt[0] == 'scene_element_size_um':
-        for size in line_elt[1].rsplit(','):
-            size_um.append(float(size))
-    if line_elt[0] == 'stack0001_size_pix':
-        for size in line_elt[1].rsplit(','):
-            size_px.append(int(size))
-args.infile.close()
-
-print size_um
-print size_px
-
-
-# generate new xuv file content
-for line in xuvdata_orig:
-    line_elt = line.rstrip().rsplit('=')
-    # scan for lines holding tile coordinates
-    if line_elt[0].endswith('abs_pos_um'):
-        prefix = line_elt[0].split('_', 1)[0]
-        tileno = int(prefix[4:8])
-        ## now calculate the new tile position
-        coord_y = tilepos[tileno - 1][0] * (1 - args.overlap)
-        coord_x = tilepos[tileno - 1][1] * (1 - args.overlap)
-        coord_y = coord_y * size_px[1] * size_um[1]
-        coord_x = coord_x * size_px[2] * size_um[2]
-        args.outfile.write(line_elt[0] + '=0,'
-            + str(coord_y) + ',' + str(coord_x) + '\n')
-    else:
-        args.outfile.write(line)
-args.outfile.close()
