dfb52297e475eafda8e248fae60d9ec0212d72ce
Niko Ehrenfeuchter <mail@he1ix.org>
2012-09-20 09:03:08 +0200

    read values as float, build data structure

--- imaris/XT/matlab/filament_parser.py
+++ imaris/XT/matlab/filament_parser.py
@@ -19,40 +19,16 @@ try:
 except IOError as e:
     argparser.error(str(e))
 
-# print args.__contains__
-# print type(args)
-# print args.infile
-# print args.outfile
-
-
-# FIXME: check code below for re-using it
 
 data = [] # the 2D-list holding our tile numbers
-tilemax = 0
 
-# parse elements of the row and discard all non-numerical ones:
-parsedata = csv.reader(args.infile, delimiter=',')
+# parse elements of the row, non-float values will raise a ValueError
+parsedata = csv.reader(args.infile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+
 for row in parsedata:
     row_num = [] # holds the converted numerical values
-    for num in row:
-        if num.isdigit():
-            tile = int(num)
-            row_num.append(tile)
-            tilemax = max(tilemax, tile)
-        else:
-            row_num.append(None)
-    # the last entry holds the maxval of this line, we discard it:
-    data.append(row_num[0:-1])
-
-# print data
-# print tilemax
-
-# construct a list of tuples with tile positions indexed by tile number
-tilepos = [()] * tilemax
-# print tilepos
-for coord_y, line in enumerate(data):
-    for coord_x, tile in enumerate(line):
-        if not tile is None:
-            tilepos[tile - 1] = (coord_y, coord_x)
-print tilepos
-
+    for val in row:
+        row_num.append(val)
+    data.append(row_num)
+print data
+print len(data)
