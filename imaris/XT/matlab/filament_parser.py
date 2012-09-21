fa6f7a553ded3aa12281eb1c91541f4d06c874de
Niko Ehrenfeuchter <mail@he1ix.org>
2012-09-21 12:24:42 +0200

    move parsing and assembling to separate function

--- imaris/XT/matlab/filament_parser.py
+++ imaris/XT/matlab/filament_parser.py
@@ -19,18 +19,38 @@ try:
 except IOError as e:
     argparser.error(str(e))
 
-# Now parse the file using the csv mechanisms to make sure we retrieve
-# float values (non-floats will raise a ValueError), then assemble a
-# 2D list of point-coordinates (tuples).
-parsedata = csv.reader(args.infile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
-data = []
-for row in parsedata:
-    num_val = []
-    for val in row:
-        num_val.append(val)
-    data.append(num_val)
-# print data
-print 'Parsed ' + str(len(data)) + ' points from CSV file.'
+def parse_float_tuples(fname):
+    """Parses every line of a CSV file into a tuple.
+
+    Parses a CSV file, makes sure all the parsed elements are float
+    values and assembles a 2D list of them, each element of the list
+    being a n-tuple holding the values of a single line from the CSV.
+
+    Args:
+        fname: A filename of a CSV file.
+
+    Returns:
+        A list of n-tuples, one tuple for each line in the CSV, for
+        example:
+
+        [[1.3, 2.7, 4.22], [22.5, 3.2, 5.5], [2.2, 8.3, 7.6]]
+
+    Raises:
+        ValueError: The parser found a non-float element in the CSV.
+    """
+    parsedata = csv.reader(fname, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+    data = []
+    for row in parsedata:
+        num_val = []
+        for val in row:
+            num_val.append(val)
+        data.append(num_val)
+    # print data
+    print 'Parsed ' + str(len(data)) + ' points from CSV file.'
+    return data
+
+
+data = parse_float_tuples(args.infile)
 
 # Calculate the full distance matrix for all points using Euklid and
 # determine the pair having the largest distance.
