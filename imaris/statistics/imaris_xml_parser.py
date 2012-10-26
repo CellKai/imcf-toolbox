c1d70bd03a502c24037c3b2172b11c975aa71d46
Niko Ehrenfeuchter <mail@he1ix.org>
2012-10-26 12:27:36 +0200

    started to objectify ImarisXML

--- imaris/statistics/imaris_xml_parser.py
+++ imaris/statistics/imaris_xml_parser.py
@@ -1,122 +1,77 @@
 #!/usr/bin/python
 
 # TODO:
-#  - create a class for handling Excel XML, move to separate package
 #  - do sanity checking
 #  - evaluate datatypes from XML cells
-#  - document functions
 
 import xml.etree.ElementTree as etree
-import sys
-import math
-import numpy as np
 
-class imaris_xml:
+class ImXMLError(Exception): pass
 
-    def __init__(self, filename):
+class ImarisXML:
 
-"""Parse Excel XML files into a python datastructure.
-"""
+    '''Parse Excel XML files into a python datastructure.
+'''
 
-def parse_xml(infile):
-    print "Processing file: " + infile
-    tree = etree.parse(infile)
-    # print "Done parsing the XML."
-    # print
-    return(tree)
+    tree = None
 
-def check_namesp(xml_etree, expected_ns):
-    real_ns = xml_etree.getroot().tag[1:].split("}")[0]
-    if not real_ns == expected_ns:
-        print "ERROR, this file doesn't have the expected XML namespace!"
-        sys.exit(1)
-    # print "Namespace parsed from XML document: '" + real_ns + "'"
-    # print
-    return(real_ns)
+    def __init__(self, xmlfile, ns=''):
+        tree = self.parse_xml(xmlfile)
+#    tree1 = parse_xml(file1)
+#    myns = check_namesp(tree1, 'urn:schemas-microsoft-com:office:spreadsheet')
+#
+#    tree2 = parse_xml(file2)
+#    myns = check_namesp(tree2, 'urn:schemas-microsoft-com:office:spreadsheet')
+#
+#    # we're looking for stuff in the "Position" worksheet:
+#    ws1_pos = get_worksheet(tree1, myns, 'Position')
+#    ws2_pos = get_worksheet(tree2, myns, 'Position')
+#
+#    cells1 = parse_celldata(ws1_pos[0], myns)
+#    cells2 = parse_celldata(ws2_pos[0], myns)
 
-def get_worksheet(xml_etree, ns, pattern):
-    pattern = ".//{%s}Worksheet[@{%s}Name='%s']" % (ns, ns, pattern)
-    worksheet = xml_etree.findall(pattern)
-    return(worksheet)
 
-def parse_celldata(worksheet, ns):
-    cells = []
-    rows = worksheet.findall('.//{%s}Row' % ns)
-    for row in rows:
-        content = []
-        # check if this is a header row:
-        style_att = '{%s}StyleID' % ns
-        if style_att in row.attrib:
-            # currently we don't process the header rows, so skip to the next
-            continue
-        # print str(len(row))
-        for cell in row:
-            content.append(cell[0].text)
-        # print content
-        cells.append(content)
-    # print cells
-    # cells is now [ [r1c1, r1c2, r1c3, ...],
-    #                [r2c1, r2c2, r2c3, ...],
-    #                [r3c1, r3c2, r3c3, ...],
-    #                ...                      ]
-    # print "Parsed rows: " + str(len(cells))
-    return(cells)
+    def parse_xml(self, infile):
+        print "Processing file: " + infile
+        tree = etree.parse(infile)
+        # print "Done parsing the XML."
+        # print
+        return(tree)
 
-def IMS_extract_coords(table_cells):
-    coords = []
-    # extract positions and ID:
-    for cell in table_cells:
-        id = int(cell[7])
-        x = float(cell[0])
-        y = float(cell[1])
-        z = float(cell[2])
-        coords.insert(id, (x, y, z))
-    # print "Parsed coordinates:", str(len(coords))
-    return(coords)
+    def check_namesp(xml_etree, expected_ns):
+        real_ns = xml_etree.getroot().tag[1:].split("}")[0]
+        if not real_ns == expected_ns:
+            print "ERROR, this file doesn't have the expected XML namespace!"
+            sys.exit(1)
+        # print "Namespace parsed from XML document: '" + real_ns + "'"
+        # print
+        return(real_ns)
 
-def main():
-    if not len(sys.argv) == 3:
-        print __doc__
-        return(1)
+    def get_worksheet(xml_etree, ns, pattern):
+        pattern = ".//{%s}Worksheet[@{%s}Name='%s']" % (ns, ns, pattern)
+        worksheet = xml_etree.findall(pattern)
+        return(worksheet)
 
-    file1 = sys.argv[1]
-    file2 = sys.argv[2]
+    def parse_celldata(worksheet, ns):
+        cells = []
+        rows = worksheet.findall('.//{%s}Row' % ns)
+        for row in rows:
+            content = []
+            # check if this is a header row:
+            style_att = '{%s}StyleID' % ns
+            if style_att in row.attrib:
+                # currently we don't process the header rows, so skip to the next
+                continue
+            # print str(len(row))
+            for cell in row:
+                content.append(cell[0].text)
+            # print content
+            cells.append(content)
+        # print cells
+        # cells is now [ [r1c1, r1c2, r1c3, ...],
+        #                [r2c1, r2c2, r2c3, ...],
+        #                [r3c1, r3c2, r3c3, ...],
+        #                ...                      ]
+        # print "Parsed rows: " + str(len(cells))
+        return(cells)
 
-    tree1 = parse_xml(file1)
-    myns = check_namesp(tree1, 'urn:schemas-microsoft-com:office:spreadsheet')
-
-    tree2 = parse_xml(file2)
-    myns = check_namesp(tree2, 'urn:schemas-microsoft-com:office:spreadsheet')
-
-    # we're looking for stuff in the "Position" worksheet:
-    ws1_pos = get_worksheet(tree1, myns, 'Position')
-    ws2_pos = get_worksheet(tree2, myns, 'Position')
-
-    cells1 = parse_celldata(ws1_pos[0], myns)
-    cells2 = parse_celldata(ws2_pos[0], myns)
-
-    # reference_spots are taken as the base to find the closest ones
-    # in the set of cand_spots
-    reference_spots = IMS_extract_coords(cells1)
-    cand_spots = IMS_extract_coords(cells2)
-
-    # test if calculation seems to be plausible:
-    # cand_spots[len(cand_spots) - 1] = (59.84, 25.602, 1.161)
-
-    for refid, refspot in enumerate(reference_spots):
-        # create a numpy array for the distances to all spots in file1
-        distances = np.empty(len(cand_spots))
-        print
-        print 'Calculating closest neighbour.'
-        print 'Original spot:  [' + str(refid) + ']', refspot
-        for idx, candspot in enumerate(cand_spots):
-            distances[idx] = dist(refspot, candspot)
-        nearest = distances.argmin()
-        print "Neighbour spot: [" + str(nearest) + ']', cand_spots[nearest]
-        print "Distance:", distances[nearest]
-    return(0)
-
-# see http://www.artima.com/weblogs/viewpost.jsp?thread=4829
-# for this nice way to handle the sys.exit()/return() calls
-if __name__ == "__main__":
-    sys.exit(main())
