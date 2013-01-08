ece3c290742883e5f0f1439a5a410014ec18ea96
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2013-01-08 17:08:20 +0100

    convex hull of a surface, first working version

--- imaris/XT/matlab/IceXTSurfacesConvexHull.m
+++ imaris/XT/matlab/IceXTSurfacesConvexHull.m
@@ -12,17 +12,17 @@ function IceXTSurfacesConvexHull()
 	conn = IceImarisConnector();
 	conn.startImaris();
 
-	% wait until the connection is ready and the user has selected some data
-    msg = 'Click "OK" to continue after opening a dataset and ' ...
-        'selecting a Surface object.'
+	% wait until the connection is ready and some data is selected
+    msg = ['Click "OK" to continue after opening a dataset and ', ...
+        'selecting a Surface object.'];
 	ans = questdlg(msg, 'Waiting for Imaris...', 'OK', 'Cancel', 'OK')
 	switch ans
 		case 'OK'
-			extractSurfaces(conn.mImarisApplication);
+			calculateSurfacesConvexHull(conn.mImarisApplication);
 	end
 end
 
-function extractSurfaces(vImApp)
+function calculateSurfacesConvexHull(vImApp)
 	vFactory = vImApp.GetFactory;
 	vSurfaces = vFactory.ToSurfaces(vImApp.GetSurpassSelection);
 	vSurpassScene = vImApp.GetSurpassScene;
@@ -45,43 +45,59 @@ function extractSurfaces(vImApp)
 		end
     end
     
+    % create a new surfaces object:
+    vSurfaceHull = vFactory.CreateSurfaces;
+    
     % NOTE: GetNumberOfSurfaces returns the number of surfaces in this
     % surface object, NOT the number of surface objects in the scene
 		
     % extract positions of surface points for each and store them
 	for SurfaceID = 0:(vSurfaces.GetNumberOfSurfaces - 1)
-		vSurfaceVertices = vSurfaces.GetVertices(SurfaceID);
-        vSurfaceTriangles = vSurfaces.GetTriangles(SurfaceID);
-		fname = sprintf('surface-%d-vertices.csv', SurfaceID);
-		csvwrite(fname, vSurfaceVertices)
-        fname = sprintf('surface-%d-triangles.csv', SurfaceID);
-        csvwrite(fname, vSurfaceTriangles)
-	end
-
+		% extract the vertices coordinates
+        vSurfaceVertices = vSurfaces.GetVertices(SurfaceID);
+        vConvexHull = convhulln(double(vSurfaceVertices));
+        
+        % For an Imaris surface objects we also need the triangles, but the
+        % convhulln() gives us just the indices of the vertex coordinates,
+        % so we need to select the edges from the original surface.
+        % first, we create an array of the size of the original vertices,
+        % setting all values to false:
+        vNumberOfPoints = size(vSurfaceVertices, 1);
+        vPoints = false(vNumberOfPoints, 1);
+        % next, set the indices of those belonging to the hull to true:
+        vPoints(vConvexHull(:)) = true;
+        % get their correspoding index number:
+        vPoints = find(vPoints);
+        % and finally use the index numbers to copy those vertices:
+        vVertices = vSurfaceVertices(vPoints, :);
+        
+        % FIXME: explain these steps in detail!
+        % remap vertex indices to our selection
+        % and reorder triangle vertices (clockwise to counter)
+        vPointsMap = zeros(vNumberOfPoints, 1);
+        vPointsMap(vPoints) = 1:numel(vPoints);
+        vTriangles = vPointsMap(vConvexHull(:, [1, 3, 2])) - 1;
+        
+        % generate normals by calculating the center and then the vectors
+        % from there to the individual vertices:
+        vMean = mean(vVertices, 1);
+        vNormals = [vVertices(:, 1) - vMean(1), ...
+                    vVertices(:, 2) - vMean(2), ...
+                    vVertices(:, 3) - vMean(3)];
+        % NOTE: it is not required to normalize the normal vectors since
+        % apparently Imaris does this for us.
+        
+        % remember the time index
+        vIndexT = vSurfaces.GetTimeIndex(SurfaceID);
+        
+        % add the new surface to the Imaris Surfaces object
+        vSurfaceHull.AddSurface(vVertices, vTriangles, vNormals, vIndexT);
+    end
+    
+    % set a name for the new Surfaces object:
+    vSurfaceHull.SetName(['Convex Hull of ', char(vSurfaces.GetName)]);
+    vSurfaceHull.SetColorRGBA(vSurfaces.GetColorRGBA);
+    vSurfaces.GetParent.AddChild(vSurfaceHull, -1)
 end
 
-% vSurfaces = vFactory.ToSurfaces(conn.mImarisApplication.GetSurpassSelection)
-% vSurfaces.GetNumberOfSurfaces
 
-% vSurfaceVertices = vSurfaces.GetVertices(0);
-% vConvexHull = convhulln(double(vSurfaceVertices));
-% vNumberOfPoints = size(vSurfaceVertices, 1)
-% vPoints = false(vNumberOfPoints, 1);
-% vPoints(vConvexHull(:)) = true
-% vPoints = find(vPoints)
-% vVertices = vSurfaceVertices(vPoints, :);
-% vPointsMap = zeros(vNumberOfPoints, 1);
-% vPointsMap(vPoints) = 1:numel(vPoints);
-% vTriangles = vPointsMap(vConvexHull(:, [1, 3, 2])) - 1;
-% vPointsMap = zeros(vNumberOfPoints, 1);
-% vPointsMap(vPoints) = 1:numel(vPoints)
-% vMean = mean(vVertices, 1)
-% vNormals = [vVertices(:, 1) - vMean(1), vVertices(:, 2) - vMean(2), ...
-%     vVertices(:, 3) - vMean(3)];
-% vIndexT = vSurfaces.GetTimeIndex(0)
-% vSurfaceHull = vFactory.CreateSurfaces
-% vSurfaceHull.AddSurface(vVertices, vTriangles, vNormals, vIndexT)
-% vSurfaceHull.SetName(['Convex Hull of "Surfaces 4"'])
-% vSurfaceHull.SetColorRGBA(vSurfaces.GetColorRGBA);
-% vSurfaceHull.SetColorRGBA(vSurfaces.GetColorRGBA)
-% vSurfaces.GetParent.AddChild(vSurfaceHull, -1)
