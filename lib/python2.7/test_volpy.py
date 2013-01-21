20b9ac94f9e93928fa527e6da7416dd2c3edae58
Niko Ehrenfeuchter <mail@he1ix.org>
2013-01-21 18:45:19 +0100

    add test script + results

---
+++ lib/python2.7/test_volpy.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+'''Test script for "volpy" module.
+
+Use like this:
+./test_volpy.py -i __testdata/fil_manual.csv > result_volpy_fil_manual.txt
+
+'''
+
+from csvtools import parse_float_tuples;
+from volpy import dist_matrix_euclidean;
+import argparse
+import pprint
+
+argparser = argparse.ArgumentParser(description=__doc__)
+argparser.add_argument('-i', '--infile', required=True, type=file,
+    help='CSV file containing filament coordinates')
+# use redirection for the moment, so disable 'outfile'
+# argparser.add_argument('-o', '--outfile', required=True, type=file,
+#     help='file to place results in')
+try:
+    args = argparser.parse_args()
+except IOError as e:
+    argparser.error(str(e))
+
+tuples_list = parse_float_tuples(args.infile)
+distance_matrix = dist_matrix_euclidean(tuples_list)
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(distance_matrix)
