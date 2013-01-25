97a2f84c93c904172fd7280b6eb84678a042abf3
Niko Ehrenfeuchter <mail@he1ix.org>
2013-01-25 17:26:37 +0100

    write results to a file in TESTDATA

--- lib/python2.7/test_filaments.py
+++ lib/python2.7/test_filaments.py
@@ -2,4 +2,14 @@
 
 from filaments import Filament
 
-flmnt = Filament(open('TESTDATA/filaments/testdata-filaments-small.csv'), debug=1)
+basedir = 'TESTDATA/filaments/'
+infile = basedir + 'testdata-filaments-small.csv'
+outfile = basedir + 'result_filaments-small.txt'
+
+flmnt = Filament(open(infile), debug=0)
+
+output = open(outfile, 'w')
+output.write(flmnt.get_coords())
+print 'Parsed %i points from "%s"' % \
+    (len(flmnt.get_coords()), infile)
+print('Written results to "%s"' % outfile)
