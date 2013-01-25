82744d3d7afe42ec71a32ed4ccb014ef8f2eab8d
Niko Ehrenfeuchter <mail@he1ix.org>
2013-01-25 11:23:33 +0100

    reflect new directory layout

--- lib/python2.7/test_ImsXMLlib.py
+++ lib/python2.7/test_ImsXMLlib.py
@@ -2,5 +2,8 @@
 
 from ImsXMLlib import ImarisXML
 
-XML = ImarisXML('TESTDATA/spots_distances/spots_red_multi_ws-all.xml', debug=1)
+basedir = 'TESTDATA/spots_distances/'
+infile = basedir + 'spots_red_multi_ws-all.xml'
+
+XML = ImarisXML(open(infile), debug=1)
 XML.celldata('Position')
