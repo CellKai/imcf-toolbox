ceb979590076c9790fe348e5826c04310d532d57
Niko Ehrenfeuchter <mail@he1ix.org>
2013-01-25 18:35:31 +0100

    reflect new directory layout

--- xuvtools/test_process-tilegrid.sh
+++ xuvtools/test_process-tilegrid.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-TILES="tiles.csv"
-SRC="unaligned.xuv"
-TGT="result_realigned.xuv"
+BASEDIR="TESTDATA"
+TILES="${BASEDIR}/tiles.csv"
+SRC="${BASEDIR}/unaligned.xuv"
+TGT="${BASEDIR}/result_realigned.xuv"
 
 # file to hold the messages sent to stdout:
-OUT="output_test_process-tilegrid.txt"
+OUT="${BASEDIR}/output_test_process-tilegrid.txt"
 
 ./process-tilegrid.py --tiles $TILES --infile $SRC --outfile $TGT --overlap 0.15 > $OUT
