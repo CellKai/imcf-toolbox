9ef50591f09a735ee27de7188b395358ec682da1
Niko Ehrenfeuchter <mail@he1ix.org>
2013-01-25 11:24:44 +0100

    write results directly to a file

--- lib/python2.7/test_ImsXMLlib.py
+++ lib/python2.7/test_ImsXMLlib.py
@@ -4,6 +4,12 @@ from ImsXMLlib import ImarisXML
 
 basedir = 'TESTDATA/spots_distances/'
 infile = basedir + 'spots_red_multi_ws-all.xml'
+outfile = basedir + 'result_ImsXMLlib_sp_red_mult_all.txt'
 
-XML = ImarisXML(open(infile), debug=1)
-XML.celldata('Position')
+print('Parsing "%s"' % infile)
+XML = ImarisXML(open(infile), debug=0)
+res = XML.celldata('Position')
+
+output = open(outfile, 'w')
+output.write(str(res))
+print('Written results to "%s"' % outfile)
