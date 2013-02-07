a7ea97daa8f178685b66d3fa143b33171e97bf0a
Niko Ehrenfeuchter <mail@he1ix.org>
2013-02-07 08:36:23 +0100

    preliminary code to show filled triangles

--- imaris/XT/matlab/filament_parser.py
+++ imaris/XT/matlab/filament_parser.py
@@ -11,8 +11,10 @@ Imaris via the XT/Matlab interface.
 import sys
 import argparse
 import matplotlib.pyplot as plt
+from matplotlib import cm
 # stuff required for matplotlib:
 from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from numpy import ma, loadtxt, asarray
 from volpy import *
 import pprint
@@ -21,7 +23,7 @@ import logging
 
 def plot3d_prep():
     fig = plt.figure()
-    return fig.gca(projection='3d')
+    return (fig.gca(projection='3d'), Axes3D(fig))
 
 def plot3d_show():
     plt.show()
@@ -105,24 +107,30 @@ def main():
     (edges, triangles, vertices) = tesselate(p2, p1, distance_matrix)
     print "vertices: %s" % vertices
 
-    poly = 0
+    polyarea = 0
+    vtxlist = []
     for (vrtx1, vrtx2, vrtx3) in triangles:
-        tri = tri_area(data[vrtx1], data[vrtx2], data[vrtx3])
-        # print tri
-        poly += tri
-    print "overall: %s" % poly
+        vtxlist.append([tuple(data[vrtx1]),
+            tuple(data[vrtx2]), tuple(data[vrtx3])])
+        polyarea += tri_area(data[vrtx1], data[vrtx2], data[vrtx3])
+    print "overall area: %s" % polyarea
+    print "vtxlist: %s" % vtxlist
 
 
     if args.plot:
         # define some colors to cycle through:
         colors = ['r', 'b', 'y', 'm', 'g']
-        plot = plot3d_prep()
+        plot, ax = plot3d_prep()
         plot3d_scatter(plot, data, 'w')
         plot3d_scatter(plot, maxdist_points, 'r', lw=18)
         plot3d_line(plot, maxdist_points, 'y')
         for p in build_tuple_seq(adjacent, cyclic=True):
             coords = [data[p[0]], data[p[1]]]
             plot3d_line(plot, coords, 'm')
+
+        for vtx in vtxlist:
+            tri = Poly3DCollection([vtx])
+            ax.add_collection3d(tri)
 
         fm1, fma1 = build_filament_mask(adjacent, maxdist_pair)
         # compressed() returns only the unmasked entries
