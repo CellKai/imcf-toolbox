479ada704693295db7fc895154d3771482cb7f1c
Niko Ehrenfeuchter <mail@he1ix.org>
2013-02-27 12:52:14 +0100

    second pushbutton, rename some widgets

--- xuvtools/qt_table_widget.py
+++ xuvtools/qt_table_widget.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'table_widget.ui'
 #
-# Created: Tue Feb 26 19:41:30 2013
+# Created: Wed Feb 27 08:40:41 2013
 #      by: PyQt4 UI code generator 4.8.5
 #
 # WARNING! All changes made in this file will be lost!
@@ -17,27 +17,42 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(538, 453)
+        MainWindow.resize(538, 463)
         MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.tableWidget = QtGui.QTableWidget(self.centralwidget)
-        self.tableWidget.setGeometry(QtCore.QRect(60, 30, 256, 192))
+        self.tableWidget.setGeometry(QtCore.QRect(10, 10, 451, 351))
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.tableWidget.sizePolicy().hasHeightForWidth())
+        self.tableWidget.setSizePolicy(sizePolicy)
+        self.tableWidget.setAutoFillBackground(False)
+        self.tableWidget.setFrameShadow(QtGui.QFrame.Sunken)
         self.tableWidget.setGridStyle(QtCore.Qt.DashLine)
         self.tableWidget.setRowCount(3)
-        self.tableWidget.setColumnCount(3)
+        self.tableWidget.setColumnCount(2)
         self.tableWidget.setObjectName(_fromUtf8("tableWidget"))
-        self.spinBox = QtGui.QSpinBox(self.centralwidget)
-        self.spinBox.setGeometry(QtCore.QRect(410, 170, 59, 27))
-        self.spinBox.setObjectName(_fromUtf8("spinBox"))
-        self.pb_plus = QtGui.QPushButton(self.centralwidget)
-        self.pb_plus.setGeometry(QtCore.QRect(390, 110, 40, 27))
-        self.pb_plus.setText(_fromUtf8("+"))
-        self.pb_plus.setObjectName(_fromUtf8("pb_plus"))
+        self.tableWidget.horizontalHeader().setDefaultSectionSize(30)
+        self.tableWidget.horizontalHeader().setMinimumSectionSize(30)
+        self.sb_x = QtGui.QSpinBox(self.centralwidget)
+        self.sb_x.setGeometry(QtCore.QRect(470, 90, 59, 27))
+        self.sb_x.setMaximum(999)
+        self.sb_x.setProperty("value", 2)
+        self.sb_x.setObjectName(_fromUtf8("sb_x"))
+        self.pb_inc_h = QtGui.QPushButton(self.centralwidget)
+        self.pb_inc_h.setGeometry(QtCore.QRect(490, 50, 40, 27))
+        self.pb_inc_h.setText(_fromUtf8("+"))
+        self.pb_inc_h.setObjectName(_fromUtf8("pb_inc_h"))
+        self.pb_dec_h = QtGui.QPushButton(self.centralwidget)
+        self.pb_dec_h.setGeometry(QtCore.QRect(490, 130, 40, 27))
+        self.pb_dec_h.setText(_fromUtf8("-"))
+        self.pb_dec_h.setObjectName(_fromUtf8("pb_dec_h"))
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
-        QtCore.QObject.connect(self.spinBox, QtCore.SIGNAL(_fromUtf8("valueChanged(int)")), self.tableWidget.insertColumn)
+        QtCore.QObject.connect(self.sb_x, QtCore.SIGNAL(_fromUtf8("valueChanged(int)")), self.tableWidget.insertColumn)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
