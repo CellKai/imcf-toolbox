fa4a2a093f6ee3fc1b48b584e7e8187128e54533
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2013-03-22 17:13:41 +0100

    convert to proper XT module
    - split it up into functions
    - make it caller-agnostic
    - add imaris meta information

--- imaris/matlab/IceXTSurfacesToSTL.m
+++ imaris/matlab/IceXTSurfacesToSTL.m
@@ -1,48 +1,82 @@
+%
+%  Surfaces to STL Exporter for Imaris 7 by Niko Ehrenfeuchter
+%
+%  Requirements:
+%	- IceImarisConnector (https://github.com/aarpon/IceImarisConnector)
+%
+%%% Imaris meta information %%%
+% <CustomTools>
+%  <Menu>
+%   <Submenu name="Surfaces Functions">
+%	<Item name="Surfaces to STL Exporter" icon="Matlab"
+%	   tooltip="Export selected Surfaces to STL.">
+%	  <Command>MatlabXT::IceXTSurfacesToSTL(%i)</Command>
+%	</Item>
+%   </Submenu>
+%  </Menu>
+%  <SurpassTab>
+%	<SurpassComponent name="bpSurfaces">
+%	  <Item name="Export Surfaces to STL">
+%		<Command>MatlabXT::IceXTSurfacesToSTL(%i)</Command>
+%	  </Item>
+%	</SurpassComponent>
+%  </SurpassTab>
+% </CustomTools>
 
-javaaddpath ImarisLib.jar;
-conn = IceImarisConnector();
-conn.startImaris();
-msg = ['Click "OK" to continue after opening a dataset and ', ...
-            'selecting a Surface object.'];
-ans = questdlg(msg, 'Waiting for Imaris...', 'OK', 'Cancel', 'OK');
-if strcmp(ans, 'Cancel')
-	return;
+function IceXTSurfacesToSTL(mImarisApplication)
+	ver = 12;	% internal version number
+	
+	if nargin == 1
+        javaaddpath ImarisLib.jar;
+		% mImarisApplication
+		conn = IceImarisConnector(mImarisApplication);
+    else
+        % start Imaris and set up the connection
+		conn = IceImarisConnector();
+		conn.startImaris();
+		msg = ['Click "OK" to continue after opening a dataset and ', ...
+					'selecting a Surface object.'];
+		ans = questdlg(msg, 'Waiting for Imaris...', 'OK', 'Cancel', 'OK');
+		if strcmp(ans, 'Cancel')
+			return;
+		end
+	end
+
+	exportSurfacesToSTL(conn.mImarisApplication);
 end
 
-vImApp = conn.mImarisApplication;
 
+function exportSurfacesToSTL(vImApp)
+	vFactory = vImApp.GetFactory;
+	vSurfaces = vFactory.ToSurfaces(vImApp.GetSurpassSelection);
 
+	% vFactory.IsSurfaces(vSurfaces)
+	% vSurfaces.GetNumberOfSurfaces
 
-vFactory = vImApp.GetFactory;
-vSurfaces = vFactory.ToSurfaces(vImApp.GetSurpassSelection);
+	vTri = vSurfaces.GetTriangles(0);
+	vNormals = vSurfaces.GetNormals(0);
+	vVertices = vSurfaces.GetVertices(0);
 
-% vFactory.IsSurfaces(vSurfaces)
-% vSurfaces.GetNumberOfSurfaces
-
-vTri = vSurfaces.GetTriangles(0);
-vNormals = vSurfaces.GetNormals(0);
-vVertices = vSurfaces.GetVertices(0);
-
-fname = 'surface.stl';
-[fname, fpath] = uiputfile(fname, 'Select a file name for the surface export');
-fid = fopen([fpath fname], 'w');
-fprintf(fid, 'solid imssurface\n');
-for tri = 1:length(vTri)
-	vi = vTri(tri,:) + 1;
-	fn = sum(vNormals(vi,:));
-	fn = fn / norm(fn);
-	tv = vVertices(vi,:);
-	fprintf(fid, ['  facet normal ' num2str(fn, '%e %e %e') '\n' ...
-	'    outer loop\n' ...
-	'      vertex ' num2str(tv(1,:), '%e %e %e') '\n' ...
-	'      vertex ' num2str(tv(2,:), '%e %e %e') '\n' ...
-	'      vertex ' num2str(tv(3,:), '%e %e %e') '\n' ...
-	'    endloop\n' ...
-	'  endfacet\n']);
-end
-fprintf(fid, 'endsolid imssurface\n');
+	fname = 'surface.stl';
+	[fname, fpath] = uiputfile(fname, 'Select a file name for the surface export');
+	fid = fopen([fpath fname], 'w');
+	fprintf(fid, 'solid imssurface\n');
+	for tri = 1:length(vTri)
+		vi = vTri(tri,:) + 1;
+		fn = sum(vNormals(vi,:));
+		fn = fn / norm(fn);
+		tv = vVertices(vi,:);
+		fprintf(fid, ['  facet normal ' num2str(fn, '%e %e %e') '\n' ...
+		'    outer loop\n' ...
+		'      vertex ' num2str(tv(1,:), '%e %e %e') '\n' ...
+		'      vertex ' num2str(tv(2,:), '%e %e %e') '\n' ...
+		'      vertex ' num2str(tv(3,:), '%e %e %e') '\n' ...
+		'    endloop\n' ...
+		'  endfacet\n']);
+	end
+	fprintf(fid, 'endsolid imssurface\n');
 	fclose(fid);
-
+end % exportSurfacesToSTL
 
 % % tri=1
 % vi = vTri(tri,:) + 1;
