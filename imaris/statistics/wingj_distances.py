8bf22cbbb469bc526144fc7cf9a3842492964c20
Niko Ehrenfeuchter <mail@he1ix.org>
2013-05-16 08:24:52 +0200

    use parameters to denote files etc.

--- imaris/statistics/wingj_distances.py
+++ imaris/statistics/wingj_distances.py
@@ -5,57 +5,88 @@ Process results of WingJ (http://www.tschaffter.ch/) with Imaris objects
 (exported from the statistics part to XML) to do distance calculations.
 '''
 
+from log import log
 import numpy as np
 import volpy as vp
 import ImsXMLlib
+import sys
+import argparse
 
-# read in the WingJ CSV files
-file_ap = open('structure_A-P.txt', 'r')
-file_vd = open('structure_V-D.txt', 'r')
-file_cnt = open('structure_contour.txt', 'r')
-structure_ap = np.loadtxt(file_ap, delimiter='\t')
-structure_vd = np.loadtxt(file_vd, delimiter='\t')
-structure_cnt = np.loadtxt(file_cnt, delimiter='\t')
-# structure_XX.shape (N, 2)
+def wingj_dist_to_surfaces(file_ap, file_vd, file_cnt, file_xml, px_size):
+    # read in the WingJ CSV files
+    structure_ap = np.loadtxt(file_ap, delimiter='\t')
+    structure_vd = np.loadtxt(file_vd, delimiter='\t')
+    structure_cnt = np.loadtxt(file_cnt, delimiter='\t')
+    # structure_XX.shape (N, 2)
 
-xmldata = ImsXMLlib.ImarisXML(open('wt1.xml', 'r'))
-wingpoints = np.array(xmldata.coordinates('Position'))
-# we're working on a projection, so we need to remove the third dimension/column
-wingpoints_2d = np.delete(wingpoints, 2, 1)
-# number of object coordinates from Imaris
-wp_nr = wingpoints_2d.shape[0]
-# wp_nr = M
+    xmldata = ImsXMLlib.ImarisXML(file_xml)
+    wingpoints = np.array(xmldata.coordinates('Position'))
+    # we're working on a projection, so we need to remove the third dimension/column
+    wingpoints_2d = np.delete(wingpoints, 2, 1)
+    # number of object coordinates from Imaris
+    wp_nr = wingpoints_2d.shape[0]
+    # wp_nr = M
 
-# calibrate WingJ data
-px_size = 0.378
-structure_ap *= px_size
-structure_vd *= px_size
-structure_cnt *= px_size
+    # calibrate WingJ data
+    # px_size = 0.378
+    structure_ap *= px_size
+    structure_vd *= px_size
+    structure_cnt *= px_size
 
-# calculate the distance matrices for all combinations
-dists_ap = vp.dist_matrix(np.vstack([wingpoints_2d, structure_ap]))
-dists_vd = vp.dist_matrix(np.vstack([wingpoints_2d, structure_vd]))
-dists_cnt = vp.dist_matrix(np.vstack([wingpoints_2d, structure_cnt]))
-# dists_XX.shape (N+M, N+M)
+    # calculate the distance matrices for all combinations
+    dists_ap = vp.dist_matrix(np.vstack([wingpoints_2d, structure_ap]))
+    dists_vd = vp.dist_matrix(np.vstack([wingpoints_2d, structure_vd]))
+    dists_cnt = vp.dist_matrix(np.vstack([wingpoints_2d, structure_cnt]))
+    # dists_XX.shape (N+M, N+M)
 
-# slice the desired parts from the distance matrices: just the rows for all
-# Imaris points ([:wp_nr,:]) and the columns for the WingJ points ([:,wp_nr:])
-wp_to_ap = dists_ap[:wp_nr, wp_nr:]
-wp_to_vd = dists_vd[:wp_nr, wp_nr:]
-wp_to_cnt = dists_cnt[:wp_nr, wp_nr:]
-#  wp_to_XX.shape (M, N)
+    # slice the desired parts from the distance matrices: just the rows for all
+    # Imaris points ([:wp_nr,:]) and the columns for the WingJ points ([:,wp_nr:])
+    wp_to_ap = dists_ap[:wp_nr, wp_nr:]
+    wp_to_vd = dists_vd[:wp_nr, wp_nr:]
+    wp_to_cnt = dists_cnt[:wp_nr, wp_nr:]
+    #  wp_to_XX.shape (M, N)
 
-# now we can just iterate through all rows finding the minimum and we get the
-# shortest distance for each point to one of the WingJ structures:
-wp_to_ap_min = np.zeros((wp_nr))
-wp_to_vd_min = np.zeros((wp_nr))
-wp_to_cnt_min = np.zeros((wp_nr))
-for i in range(wp_nr):
-    wp_to_ap_min[i] = wp_to_ap[i].min()
-    wp_to_vd_min[i] = wp_to_vd[i].min()
-    wp_to_cnt_min[i] = wp_to_cnt[i].min()
+    # now we can just iterate through all rows finding the minimum and we get the
+    # shortest distance for each point to one of the WingJ structures:
+    wp_to_ap_min = np.zeros((wp_nr))
+    wp_to_vd_min = np.zeros((wp_nr))
+    wp_to_cnt_min = np.zeros((wp_nr))
+    for i in range(wp_nr):
+        wp_to_ap_min[i] = wp_to_ap[i].min()
+        wp_to_vd_min[i] = wp_to_vd[i].min()
+        wp_to_cnt_min[i] = wp_to_cnt[i].min()
 
-# export the results as CSV files
-np.savetxt('wt1-to-AP.csv', wp_to_ap_min, delimiter=',')
-np.savetxt('wt1-to-VD.csv', wp_to_vd_min, delimiter=',')
-np.savetxt('wt1-to-contour.csv', wp_to_cnt_min, delimiter=',')
+    # export the results as CSV files
+    np.savetxt('wt1-to-AP.csv', wp_to_ap_min, delimiter=',')
+    np.savetxt('wt1-to-VD.csv', wp_to_vd_min, delimiter=',')
+    np.savetxt('wt1-to-contour.csv', wp_to_cnt_min, delimiter=',')
+
+def main():
+    argparser = argparse.ArgumentParser(description=__doc__)
+    argparser.add_argument('--ap', required=True, type=file,
+        help='WingJ structure file for the A-P separation.')
+    argparser.add_argument('--vd', required=True, type=file,
+        help='WingJ structure file for the V-D separation.')
+    argparser.add_argument('--contour', required=True, type=file,
+        help='WingJ structure file for the contour line.')
+    argparser.add_argument('--imsxml', required=True, type=file,
+        help='Imaris Excel XML export containing a "Position" sheet.')
+    argparser.add_argument('-p', '--pixelsize', required=False, type=float,
+        default=1.0, help='Pixel size to calibrate WingJ data.')
+    argparser.add_argument('-v', '--verbosity', dest='verbosity',
+        action='count', default=0)
+    try:
+        args = argparser.parse_args()
+    except IOError as e:
+        argparser.error(str(e))
+
+    # default loglevel is 30 while 20 and 10 show more details
+    loglevel = (3 - args.verbosity) * 10
+    log.setLevel(loglevel)
+
+    wingj_dist_to_surfaces(args.ap, args.vd,
+        args.contour, args.imsxml, args.pixelsize)
+
+
+if __name__ == "__main__":
+        sys.exit(main())
