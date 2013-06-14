9f3ad4ff1b08f16533bc1ca367780724b7d06427
Niko Ehrenfeuchter <mail@he1ix.org>
2013-06-14 16:15:58 +0200

    initialize newly created cells

--- xuvtools/table_widget.py
+++ xuvtools/table_widget.py
@@ -325,12 +325,18 @@ class My_UI_Window(Ui_MainWindow):
         # instead, it would be done at the left/top):
         for i in range(dcols):
             self.tableWidget.insertColumn(self.cols)
+            # initialize the new column of cells:
+            for row in range(self.rows):
+                self.gen_cell(row, self.cols)
         for i in range(dcols * -1):
             self.tableWidget.removeColumn(self.cols - 1)
 
         # now adjust the rows at the bottom of the table (see above):
         for i in range(drows):
             self.tableWidget.insertRow(self.rows)
+            # initialize the new row of cells:
+            for col in range(self.cols):
+                self.gen_cell(self.rows, col)
         for i in range(drows * -1):
             self.tableWidget.removeRow(self.rows - 1)
         self.rows = self.tableWidget.rowCount()
