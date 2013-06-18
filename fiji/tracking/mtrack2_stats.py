50b05617c017d0cad55d3957ad2994bce101953f
Niko Ehrenfeuchter <mail@he1ix.org>
2013-06-18 17:28:16 +0200

    add check_filehandle() for future filename/handle agnostic behaviour

--- fiji/tracking/mtrack2_stats.py
+++ fiji/tracking/mtrack2_stats.py
@@ -8,6 +8,7 @@ by the "MTrack2" plugin for ImageJ/FiJi.
 #  - rewrite this code so it can be imported in other scripts
 #  - then import it into the GUI script instead of "calling" it from there
 #  - check which functions should go into a central module
+#  - move check_filehandle() ASAP!
 
 import csv
 import sys
@@ -69,6 +70,42 @@ def save_results(f_out, data, labeled=False):
         save_results_labeled(f_out, data, lbl)
     else:
         save_results_unlabeled(f_out, data)
+
+def check_filehandle(filename, mode):
+    '''Make sure a variable is either a filehandle or create one from it.
+    .
+    This function takes a variable and checks whether it is already a
+    filehandle with the desired mode or a string that can be turned into
+    a filehandle with that mode. This can be used e.g. to make functions
+    agnostic against being supplied a file-type parameter that was gathered
+    via argparse (then it's already a filehandle) or as a plain string.
+    .
+    Parameters
+    ----------
+    filename : str or filehandle
+    mode : str
+        The desired mode of the filehandle.
+    .
+    Returns
+    -------
+    A valid (open) filehandle with the given mode. Raises an IOError
+    otherwise.
+    '''
+    log.debug(type(filename))
+    if (type(filename).__name__ == 'str'):
+        try:
+            return open(filename, mode)
+        except IOError as e:
+            message = "can't open '%s': %s"
+            raise SystemExit(message % (filename, e))
+    elif (type(filename).__name__ == 'file'):
+        if (filename.mode != mode):
+            message = "mode mismatch: %s != %s"
+            raise IOError(message % (filename.mode, mode))
+        return filename
+    else:
+        message = "unknown data type (expected string or filehandle): %s"
+        raise SystemExit(message % type(filename))
 
 def main():
     argparser = argparse.ArgumentParser(description=__doc__)
