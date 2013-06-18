eae519fbc4119420a304fc75e471e702cff8e8ea
Niko Ehrenfeuchter <mail@he1ix.org>
2013-06-18 13:55:39 +0200

    clean outfiles before testing
    to make sure we don't miss the case when the tool doesn't
    touch the outfile at all, e.g. because it fails before...

--- fiji/tracking/test_mtrack2_stats.sh
+++ fiji/tracking/test_mtrack2_stats.sh
@@ -5,5 +5,6 @@ BASEDIR="TESTDATA/mtrack2"
 for infile in $(ls $BASEDIR/*.txt) ; do
     fname=$(basename $infile)
     outfile="results__$(echo $fname | sed 's,\.txt$,.csv,')"
+    rm -f $BASEDIR/$outfile
     ./mtrack2_stats.py --infile $BASEDIR/$fname --outfile $BASEDIR/$outfile
 done
