6df04cde2a3a51264fc3a744ffeb7851fddd0ae2
Niko Ehrenfeuchter <mail@he1ix.org>
2013-07-12 10:41:43 +0200

    fix test script to use "log" module
    this adapts the test script to the changes to ImsXMLlib from
    commit:af75d8279e3616208505acf649e23c0a9d01b61b

--- lib/python2.7/test_ImsXMLlib.py
+++ lib/python2.7/test_ImsXMLlib.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 
 from ImsXMLlib import ImarisXML
+from log import log
+
+log.setLevel(20)
 
 basedir = 'TESTDATA/spots_distances/'
 infile = basedir + 'spots_red_multi_ws-all.xml'
 outfile = basedir + 'result_ImsXMLlib_sp_red_mult_all.txt'
 
-print('Parsing "%s"' % infile)
-XML = ImarisXML(open(infile), debug=0)
+XML = ImarisXML(open(infile))
 res = XML.celldata('Position')
 
 output = open(outfile, 'w')
