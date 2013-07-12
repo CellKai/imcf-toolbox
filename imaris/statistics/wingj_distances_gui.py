6803c90653c7e6947b8df5571b999646fd3169ed
Niko Ehrenfeuchter <mail@he1ix.org>
2013-07-12 17:37:39 +0200

    use lambda to avoid duplicating slot functions

--- imaris/statistics/wingj_distances_gui.py
+++ imaris/statistics/wingj_distances_gui.py
@@ -9,6 +9,22 @@ import sys
 from wingj_distances import wingj_dist_to_surfaces
 from ui_generic_in4_out3_spin import *
 
+def select_file(element):
+    """Show file dialog and update an elements text with the result.
+
+    This is a callback function for usage with "Browse" buttons or similar in a
+    PyQt GUI. It displays the system's file selection dialog and updates the
+    supplied GUI element with the result. To use it with Qt's connect() method,
+    a lambda function must be used there, e.g.
+
+    >>> connect(btn, sig, lambda elt=self.line_edit: select_file(elt))
+
+    The main purpose of this is to eliminate the need for implementing a
+    separate callback function for each slot, resulting in many more or less
+    identical code segments.
+    """
+    element.setText(QtGui.QFileDialog.getOpenFileName())
+
 
 class My_UI_Window(Ui_MainWindow):
     """Main Window for WingJ GUI."""
@@ -30,40 +46,26 @@ class My_UI_Window(Ui_MainWindow):
         self.le_outfile_3.setPlaceholderText(msg % 'contour')
         MainWindow.addAction(self.sc_ctrl_w)
         MainWindow.addAction(self.sc_ctrl_q)
-        QtCore.QObject.connect(self.pb_infile, QtCore.SIGNAL("clicked()"), self.selectInfile)
-        QtCore.QObject.connect(self.pb_infile_2, QtCore.SIGNAL("clicked()"), self.selectInfile_2)
-        QtCore.QObject.connect(self.pb_infile_3, QtCore.SIGNAL("clicked()"), self.selectInfile_3)
-        QtCore.QObject.connect(self.pb_infile_4, QtCore.SIGNAL("clicked()"), self.selectInfile_4)
-        QtCore.QObject.connect(self.pb_outfile, QtCore.SIGNAL("clicked()"), self.selectOutfile)
-        QtCore.QObject.connect(self.pb_outfile_2, QtCore.SIGNAL("clicked()"), self.selectOutfile_2)
-        QtCore.QObject.connect(self.pb_outfile_3, QtCore.SIGNAL("clicked()"), self.selectOutfile_3)
+        QtCore.QObject.connect(self.pb_infile, QtCore.SIGNAL("clicked()"),
+            lambda elt=self.le_infile: select_file(elt))
+        QtCore.QObject.connect(self.pb_infile_2, QtCore.SIGNAL("clicked()"),
+            lambda elt=self.le_infile_2: select_file(elt))
+        QtCore.QObject.connect(self.pb_infile_3, QtCore.SIGNAL("clicked()"),
+            lambda elt=self.le_infile_3: select_file(elt))
+        QtCore.QObject.connect(self.pb_infile_4, QtCore.SIGNAL("clicked()"),
+            lambda elt=self.le_infile_4: select_file(elt))
+        QtCore.QObject.connect(self.pb_outfile, QtCore.SIGNAL("clicked()"),
+            lambda elt=self.le_outfile: select_file(elt))
+        QtCore.QObject.connect(self.pb_outfile_2, QtCore.SIGNAL("clicked()"),
+            lambda elt=self.le_outfile_2: select_file(elt))
+        QtCore.QObject.connect(self.pb_outfile_3, QtCore.SIGNAL("clicked()"),
+            lambda elt=self.le_outfile_3: select_file(elt))
         QtCore.QObject.connect(self.bb_ok_cancel, QtCore.SIGNAL("rejected()"), MainWindow.close)
         QtCore.QObject.connect(self.bb_ok_cancel, QtCore.SIGNAL("accepted()"), self.runTool)
         QtCore.QObject.connect(self.sc_ctrl_w, QtCore.SIGNAL("triggered()"), MainWindow.close)
         QtCore.QObject.connect(self.sc_ctrl_q, QtCore.SIGNAL("triggered()"), MainWindow.close)
         QtCore.QObject.connect(self.sl_verbosity, QtCore.SIGNAL("valueChanged(int)"), self.sb_verbosity.setValue)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def selectInfile(self):
-        self.le_infile.setText(QtGui.QFileDialog.getOpenFileName())
-
-    def selectInfile_2(self):
-        self.le_infile_2.setText(QtGui.QFileDialog.getOpenFileName())
-
-    def selectInfile_3(self):
-        self.le_infile_3.setText(QtGui.QFileDialog.getOpenFileName())
-
-    def selectInfile_4(self):
-        self.le_infile_4.setText(QtGui.QFileDialog.getOpenFileName())
-
-    def selectOutfile(self):
-        self.le_outfile.setText(QtGui.QFileDialog.getOpenFileName())
-
-    def selectOutfile_2(self):
-        self.le_outfile_2.setText(QtGui.QFileDialog.getOpenFileName())
-
-    def selectOutfile_3(self):
-        self.le_outfile_3.setText(QtGui.QFileDialog.getOpenFileName())
 
     def runTool(self):
         """Collect the settings and launch the calculation."""
