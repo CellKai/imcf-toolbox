c5f1d7babec21575057411828124e13cee32f772
Niko Ehrenfeuchter <mail@he1ix.org>
2013-08-01 16:26:57 +0200

    add Qt callback function to select an existing directory

--- lib/python2.7/genui/__init__.py
+++ lib/python2.7/genui/__init__.py
@@ -45,3 +45,29 @@ def select_file_save(element, cap=None, ffilter=''):
         cap='Save As'
     element.setText(QtGui.QFileDialog.getSaveFileName(
         caption=cap, filter=ffilter))
+
+
+def select_directory(element, cap=None, directory='', dirsonly=True):
+    """Show directory dialog and update an element's text with the result.
+
+    Provides a callback function for usage with "Browse" buttons or similar in
+    a PyQt GUI. It displays the system's file selection dialog and lets the
+    user choose an existing directory.
+
+    Its purpose is the elimination of separate callback functions for each
+    slot, resulting in many more or less identical code segments. To use it
+    with Qt's connect() method, a lambda function must be used there, e.g.
+
+    >>> connect(btn, sig, lambda elt=self.line_edit: select_file(elt))
+    """
+    if cap is None:
+        cap='Select Directory'
+    # TODO: this has no effect on Linux, test it on Windows and decide whether
+    # to keep or discard it:
+    if dirsonly:
+        options = QtGui.QFileDialog.ShowDirsOnly
+        element.setText(QtGui.QFileDialog.getExistingDirectory(
+            caption=cap, directory=directory, options=options))
+    else:
+        element.setText(QtGui.QFileDialog.getExistingDirectory(
+            caption=cap, directory=directory))
