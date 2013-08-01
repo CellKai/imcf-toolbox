83b1bbb05c6831392cda2bfc563f0288cf885488
Niko Ehrenfeuchter <mail@he1ix.org>
2013-08-01 16:58:40 +0200

    fixup pyuic4 script for new path and naming scheme

--- ui/Qt/update.sh
+++ ui/Qt/update.sh
@@ -4,8 +4,8 @@ echo "Updating generated Python code..."
 
 for file in *.ui ; do
     echo " - $file"
-    TGT="ui_$(echo $file | sed 's,.ui$,.py,')"
-    pyuic4 $file -o ../../lib/python2.7/$TGT
+    TGT=$(echo $file | sed 's,^generic_,, ; s,.ui$,.py,')
+    pyuic4 $file -o ../../lib/python2.7/genui/$TGT
 done
 
 echo "Done."
