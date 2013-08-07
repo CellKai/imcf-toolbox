8fd44c47581777c0c946ed062cb9379917eb5b71
Niko Ehrenfeuchter <mail@he1ix.org>
2013-08-07 17:08:29 +0200

    fix bug in documentation

--- lib/python2.7/volpy/imagej.py
+++ lib/python2.7/volpy/imagej.py
@@ -17,7 +17,7 @@ def read_csv_com(fname):
     fname : str or filehandle
         The CSV export from an ImageJ measurement. Needs to contain the results
         for center-of-mass ('XM' and 'YM' columns). Make sure to set the output
-        option in ImageJ to either '.csv' or '.xls' but *NOT* '.csv' as the
+        option in ImageJ to either '.csv' or '.xls' but *NOT* '.txt' as the
         latter will use tabs as delimiter.
 
     Returns
