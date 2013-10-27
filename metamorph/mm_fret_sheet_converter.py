f9f4426ccc7285bd312d9cfd295110008809f582
Niko Ehrenfeuchter <mail@he1ix.org>
2013-10-27 18:49:45 +0100

    fix sample data filename

--- metamorph/mm_fret_sheet_converter.py
+++ metamorph/mm_fret_sheet_converter.py
@@ -1,11 +1,12 @@
-from mmap import mmap,ACCESS_READ
+#!/usr/bin/python
+
 from xlrd import open_workbook, colname
 import numpy as np
 import pprint
 
 ppr = pprint.PrettyPrinter(indent=4)
 
-xlfile = open('2013_08_05T2_-_Linescans_example_workingcopy.xlsx','rb').read()
+xlfile = open('../sample_data/fret/2013_08_05T2_-_Linescans_example_2013_10_15.xlsx','rb').read()
 wb = open_workbook(file_contents=xlfile)
 
 sheet = wb.sheet_by_index(0)
@@ -15,8 +16,6 @@ print("Sheet 0 name: %s" % sheet.name)
 sec_size = int(sheet.cell_value(1,8))
 sections = int(sheet.nrows / (sec_size + 3))
 print("Section size: %s, number of sections: %s" % (sec_size, sections))
-
-print colname(3)
 
 for sec in range(sections):
     print sheet.row_values(1,1)
