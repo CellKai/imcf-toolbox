4a0739c1ed408a675c2d03422f23cc5ef7a91ab0
Niko Ehrenfeuchter <mail@he1ix.org>
2013-10-27 19:24:30 +0100

    add main(), use cmdline arguments

--- metamorph/mm_fret_sheet_converter.py
+++ metamorph/mm_fret_sheet_converter.py
@@ -3,6 +3,8 @@
 from xlrd import open_workbook, colname
 import numpy as np
 import argparse
+import sys
+
 
 def parse_arguments():
     """Parse commandline arguments."""
@@ -17,17 +19,24 @@ def parse_arguments():
         argparser.error(str(err))
     return args
 
-xlfile = open('../sample_data/fret/2013_08_05T2_-_Linescans_example_2013_10_15.xlsx','rb').read()
-wb = open_workbook(file_contents=xlfile)
+def main():
+    """Parse commandline arguments and run reader."""
+    args = parse_arguments()
 
-sheet = wb.sheet_by_index(0)
-print("Sheet 0 name: %s" % sheet.name)
+    xlfile = open(args.infile,'rb').read()
+    wb = open_workbook(file_contents=xlfile)
 
-sec_size = int(sheet.cell_value(1,8))
-sections = int(sheet.nrows / (sec_size + 3))
-print("Section size: %s, number of sections: %s" % (sec_size, sections))
+    sheet = wb.sheet_by_index(0)
+    print("Sheet 0 name: %s" % sheet.name)
 
-for sec in range(sections):
-    print sheet.row_values(1,1)
+    # sec_size = 83
+    sec_size = int(sheet.cell_value(1,8))
+    sections = int(sheet.nrows / (sec_size + 3))
+    print("Section size: %s, number of sections: %s" % (sec_size, sections))
+
+    for sec in range(sections):
+        print sheet.row_values(1,1)
 
 
+if __name__ == "__main__":
+    sys.exit(main())
