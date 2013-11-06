d548ca6a2c25da9e62a28988f44b7e5973a2d52a
Niko Ehrenfeuchter <mail@he1ix.org>
2013-11-06 18:40:43 +0100

    wrap into main()

--- imaris/statistics/positions_2d_to_bitmap.py
+++ imaris/statistics/positions_2d_to_bitmap.py
@@ -10,35 +10,43 @@ ImageJ via the File>Import>Text_Image function, empty pixels are black, the
 others contain values > 0.
 """
 
+import sys
+
 import imaris_xml
 import numpy as np
 
-# TODO: use arg
-dim_x = 256
-dim_y = dim_x
-offset = 50
+def main():
+    """Read Imaris export and generate bitmap."""
+    # TODO: use arg
+    dim_x = 256
+    dim_y = dim_x
+    offset = 50
 
-# TODO: use arg
-fh = open('spots_red_multi_ws-all.xml', 'r')
-xmldata = imaris_xml.ImarisXML(fh)
-    
-coords = xmldata.coordinates('Position')
-coords_2d = coords[:,0:2]
+    # TODO: use arg
+    fh = open('spots_red_multi_ws-all.xml', 'r')
+    xmldata = imaris_xml.ImarisXML(fh)
 
-# TODO: make this configurable
-# remove emtpy blocks (aka shift coords to origin)
-coords_2d[:,0] -= coords_2d[:,0].min()
-coords_2d[:,1] -= coords_2d[:,1].min()
+    coords = xmldata.coordinates('Position')
+    coords_2d = coords[:,0:2]
 
-xmax = coords_2d[:,0].max()
-ymax = coords_2d[:,1].max()
+    # TODO: make this configurable
+    # remove emtpy blocks (aka shift coords to origin)
+    coords_2d[:,0] -= coords_2d[:,0].min()
+    coords_2d[:,1] -= coords_2d[:,1].min()
 
-matrix = np.zeros((dim_x, dim_y), dtype=np.int)
-for point in coords_2d:
-    pix_x = int((point[0] / xmax) * (dim_x - 1))
-    pix_y = int((point[1] / ymax) * (dim_y - 1))
-    # print "(%f,%f) -> (%i,%i)" % (point[0], point[1], pix_x, pix_y)
-    matrix[pix_x, pix_y] += offset
-    
-# TODO: use arg
-np.savetxt('bitmap.csv', matrix, fmt='%i')
+    xmax = coords_2d[:,0].max()
+    ymax = coords_2d[:,1].max()
+
+    matrix = np.zeros((dim_x, dim_y), dtype=np.int)
+    for point in coords_2d:
+        pix_x = int((point[0] / xmax) * (dim_x - 1))
+        pix_y = int((point[1] / ymax) * (dim_y - 1))
+        # print "(%f,%f) -> (%i,%i)" % (point[0], point[1], pix_x, pix_y)
+        matrix[pix_x, pix_y] += offset
+
+    # TODO: use arg
+    np.savetxt('bitmap.csv', matrix, fmt='%i')
+
+
+if __name__ == "__main__":
+    sys.exit(main())
