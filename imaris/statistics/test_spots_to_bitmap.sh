3e9aa7a919d93d506b663bfa67e2c81fb4a74f9b
Niko Ehrenfeuchter <mail@he1ix.org>
2013-11-11 11:18:31 +0100

    display effective commands on execution

--- imaris/statistics/test_spots_to_bitmap.sh
+++ imaris/statistics/test_spots_to_bitmap.sh
@@ -2,10 +2,14 @@
 
 INFILE="TESTDATA/spots_distances/spots_nocorners_ws-all.xml"
 PREFIX="TESTDATA/spots_distances/spots_nocorners_bitmap"
+set -x
 ./spots_to_bitmap.py --size 256 -i $INFILE -o ${PREFIX}-cropped.csv --crop
 ./spots_to_bitmap.py --size 256 -i $INFILE -o ${PREFIX}-uncropped.csv 
+set +x
 
 INFILE="TESTDATA/spots_distances/spots_allcorners_ws-all.xml"
 PREFIX="TESTDATA/spots_distances/spots_allcorners_bitmap"
+set -x
 ./spots_to_bitmap.py --size 256 -i $INFILE -o ${PREFIX}-cropped.csv --crop
 ./spots_to_bitmap.py --size 256 -i $INFILE -o ${PREFIX}-uncropped.csv 
+set +x
