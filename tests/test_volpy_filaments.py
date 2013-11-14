8faf2b2c8ab1fbd1fc73069eac5e9bf234ecd9b0
Niko Ehrenfeuchter <mail@he1ix.org>
2013-11-14 18:51:14 +0100

    update test script for more files
    and also adjust the output filename to better reflect the content

--- tests/test_volpy_filaments.py
+++ tests/test_volpy_filaments.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python
 
+"""Tests the volpy filament parser."""
+
 from volpy import Filament
 from log import set_loglevel
 
+
+def run_test(fin, fout):
+    flmnt = Filament(fin)
+
+    output = open(fout, 'w')
+    output.write(str(flmnt.get_coords()))
+    print 'Parsed %i points from "%s"' % \
+        (len(flmnt.get_coords()), fin)
+    print('Written results to "%s"' % fout)
+
+set_loglevel(0)
 basedir = 'TESTDATA/filaments/'
+
 infile = basedir + 'testdata-filaments-small.csv'
-outfile = basedir + 'result_filaments-small.txt'
+outfile = basedir + 'result_filaments-small.coords.txt'
+run_test(infile, outfile)
 
-set_loglevel(2)
-flmnt = Filament(infile)
+infile = basedir + 'testdata-filaments-manual.csv'
+outfile = basedir + 'result_filaments-manual.coords.txt'
+run_test(infile, outfile)
 
-output = open(outfile, 'w')
-output.write(str(flmnt.get_coords()))
-print 'Parsed %i points from "%s"' % \
-    (len(flmnt.get_coords()), infile)
-print('Written results to "%s"' % outfile)
+infile = basedir + 'testdata-junction-wt-001.csv'
+outfile = basedir + 'result_junction-wt-001.coords.txt'
+run_test(infile, outfile)
