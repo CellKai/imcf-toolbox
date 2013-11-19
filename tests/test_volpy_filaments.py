47850a9b81635ebeb97a759e4c75bc2181f0cc14
Niko Ehrenfeuchter <mail@he1ix.org>
2013-11-19 10:51:28 +0100

    use logfiles for stderr logging

--- tests/test_volpy_filaments.py
+++ tests/test_volpy_filaments.py
@@ -6,7 +6,8 @@ from volpy import Filament
 from log import set_loglevel, set_filehandler
 
 
-def run_test(fin, fout):
+def run_test(fin, fout, flog):
+    set_filehandler(flog, no_stderr=True, mode='w')
     flmnt = Filament(fin)
 
     output = open(fout, 'w')
@@ -15,18 +16,20 @@ def run_test(fin, fout):
         (len(flmnt.get_coords()), fin)
     print('Written results to "%s"' % fout)
 
-set_filehandler('/tmp/asdfasdfasdf', no_stdout=True)
 set_loglevel(2)
 basedir = 'TESTDATA/filaments/'
 
 infile = basedir + 'testdata-filaments-small.csv'
 outfile = basedir + 'result_filaments-small.coords.txt'
-run_test(infile, outfile)
+logfile = basedir + 'result_filaments-small.stderr.txt'
+run_test(infile, outfile, logfile)
 
 infile = basedir + 'testdata-filaments-manual.csv'
 outfile = basedir + 'result_filaments-manual.coords.txt'
-run_test(infile, outfile)
+logfile = basedir + 'result_filaments-manual.stderr.txt'
+run_test(infile, outfile, logfile)
 
 infile = basedir + 'testdata-junction-wt-001.csv'
 outfile = basedir + 'result_junction-wt-001.coords.txt'
-run_test(infile, outfile)
+logfile = basedir + 'result_junction-wt-001.stderr.txt'
+run_test(infile, outfile, logfile)
