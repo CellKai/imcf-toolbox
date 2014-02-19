ce4701c9f5f3af478b94e8f3b98f1b1e89378334
Niko Ehrenfeuchter <mail@he1ix.org>
2014-02-19 18:48:01 +0100

    Generate and print macro code.

--- fiji/plugins/sandbox/fluoview_parsing.py
+++ fiji/plugins/sandbox/fluoview_parsing.py
@@ -13,4 +13,10 @@ set_loglevel(1)
 base = '/home/ehrenfeu/usr/packages/imcf_toolbox/sample_data/fluoview/minimal_1mosaic_15pct/'
 mf = base + 'MATL_Mosaic.log'
 
-print(mosaic.gen_stitching_macro_code())
+mosaic = fv.FluoViewMosaic(mf)
+code = mosaic.gen_stitching_macro_code('stitching')
+flat = ""
+for line in code:
+	flat += line
+
+print flat
