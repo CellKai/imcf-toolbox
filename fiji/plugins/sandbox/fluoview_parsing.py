8d5ad4b6061963d42bffb53abfd1d28d1aee6899
Niko Ehrenfeuchter <mail@he1ix.org>
2014-02-19 18:47:19 +0100

    Clean up testcode.

--- fiji/plugins/sandbox/fluoview_parsing.py
+++ fiji/plugins/sandbox/fluoview_parsing.py
@@ -1,17 +1,16 @@
 from sys import path
-from java.lang.System import getProperty
+# from java.lang.System import getProperty
  
 # extend the search path by $FIJI_ROOT/bin/
 path.append('/home/ehrenfeu/.local/lib/python2.7')
 path.append('/home/ehrenfeu/.local/lib/python2.7/site-packages/volpy')
 
-# print(path)
 import fluoview as fv
 from log import log, set_loglevel
 
-set_loglevel(5)
-log.warn("wohin?")
+set_loglevel(1)
 
-mf = '/home/ehrenfeu/usr/packages/imcf_toolbox/sample_data/fluoview/minimal_1mosaic_15pct/MATL_Mosaic.log'
+base = '/home/ehrenfeu/usr/packages/imcf_toolbox/sample_data/fluoview/minimal_1mosaic_15pct/'
+mf = base + 'MATL_Mosaic.log'
 
 print(mosaic.gen_stitching_macro_code())
