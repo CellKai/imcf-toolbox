329ca9f78ad9e8e822b5d860690d19790681a62d
Niko Ehrenfeuchter <mail@he1ix.org>
2014-02-26 17:35:11 +0100

    Add FluoView stitcher plugin java wrapper.

---
+++ fiji/plugins/IMCF_Tools/FluoView_OIF_Stitcher.java
@@ -0,0 +1,14 @@
+import ij.IJ;
+
+import ij.plugin.PlugIn;
+
+public class FluoView_OIF_Stitcher implements PlugIn {
+	public void run(String arg) {
+		try {
+			new Jython.Refresh_Jython_Scripts().runScript(getClass()
+				.getResource("FluoView_OIF_Stitcher.py").openStream());
+		} catch (Exception e) {
+			IJ.handleException(e);
+		}
+	}
+}
