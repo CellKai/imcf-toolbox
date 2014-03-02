fade70475b11d4e497642a3d8cad886c409c6249
Niko Ehrenfeuchter <mail@he1ix.org>
2014-03-02 18:08:58 +0100

    Rename macros to better reflect what they do.

--- fiji/tools/Masked_Intensity_Measurements.java
+++ fiji/tools/Masked_Intensity_Measurements.java
@@ -2,11 +2,11 @@ import ij.IJ;
 
 import ij.plugin.PlugIn;
 
-public class Stitch_all_files_in_directory implements PlugIn {
+public class Masked_Intensity_Measurements implements PlugIn {
 	public void run(String arg) {
 		try {
 			new CLI.Refresh_Macros().runScript(getClass()
-				.getResource("Stitch_all_files_in_directory.ijm").openStream());
+				.getResource("Masked_Intensity_Measurements.ijm").openStream());
 		} catch (Exception e) {
 			IJ.handleException(e);
 		}
--- fiji/tools/Stitch_directory_with_LSMs.java
+++ fiji/tools/Stitch_directory_with_LSMs.java
@@ -2,11 +2,11 @@ import ij.IJ;
 
 import ij.plugin.PlugIn;
 
-public class Measurements_2d_2ch implements PlugIn {
+public class Stitch_directory_with_LSMs implements PlugIn {
 	public void run(String arg) {
 		try {
 			new CLI.Refresh_Macros().runScript(getClass()
-				.getResource("Measurements_2d_2ch.ijm").openStream());
+				.getResource("Stitch_directory_with_LSMs.ijm").openStream());
 		} catch (Exception e) {
 			IJ.handleException(e);
 		}
