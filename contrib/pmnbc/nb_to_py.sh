74d1cb7360c70a4402e5bd0916bd385748d88551
Niko Ehrenfeuchter <mail@he1ix.org>
2014-04-15 14:26:02 +0200

    Add imports, main frame and some aliases.

--- contrib/pmnbc/nb_to_py.sh
+++ contrib/pmnbc/nb_to_py.sh
@@ -2,6 +2,15 @@
 #
 # Poor Man's Netbeans Converter
 
+cat - << EOF
+import javax.swing
+
+jFrame = javax.swing.JFrame()
+getContentPane = jFrame.getContentPane
+pack = jFrame.pack
+
+EOF
+
 sed 's,^        ,, ;
     s,;,, ;
     s,^[a-zA-Z0-9\.]\+ \([a-zA-Z0-9\.]\+ =\) new ,\1 , ;
