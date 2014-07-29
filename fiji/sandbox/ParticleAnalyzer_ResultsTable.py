e39da29ee8708bf5ad9951b4b8c0a3d5f73109c1
Niko Ehrenfeuchter <mail@he1ix.org>
2014-07-29 16:45:38 +0200

    Sample code for working with the ParticleAnalyzer and ResultsTable.

---
+++ fiji/sandbox/ParticleAnalyzer_ResultsTable.py
@@ -0,0 +1,30 @@
+from ij import IJ
+from ij.plugin.frame import RoiManager
+from ij.plugin.filter import ParticleAnalyzer
+from ij.measure import ResultsTable, Measurements
+from java.lang import Double
+
+
+imp = IJ.getImage()
+
+# Create a table to store the results
+table = ResultsTable()
+# Create a hidden ROI manager, to store a ROI for each blob or cell
+roim = RoiManager(True)
+# Create a ParticleAnalyzer, with arguments:
+# 1. options (could be SHOW_ROI_MASKS, SHOW_OUTLINES, SHOW_MASKS, SHOW_NONE, ADD_TO_MANAGER, and others; combined with bitwise-or)
+# 2. measurement options (see [http://rsb.info.nih.gov/ij/developer/api/ij/measure/Measurements.html Measurements])
+# 3. a ResultsTable to store the measurements
+# 4. The minimum size of a particle to consider for measurement
+# 5. The maximum size (idem)
+# 6. The minimum circularity of a particle
+# 7. The maximum circularity
+pa = ParticleAnalyzer(ParticleAnalyzer.ADD_TO_MANAGER,
+	Measurements.AREA, table, 0,
+	Double.POSITIVE_INFINITY, 0.0, 1.0)
+pa.setHideOutputImage(True)
+ 
+if pa.analyze(imp):
+  print "All ok"
+else:
+  print "There was a problem in analyzing", blobs
