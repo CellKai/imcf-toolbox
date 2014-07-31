0a2ad2abeb94dc09a81776711db6b7e9383cf91a
Niko Ehrenfeuchter <mail@he1ix.org>
2014-07-31 17:36:46 +0200

    Rename and clean up commandline parameters.

--- contrib/imaris/junction_statistics.py
+++ contrib/imaris/junction_statistics.py
@@ -19,8 +19,8 @@ from log import set_loglevel
 def parse_arguments():
     """Parse the commandline arguments."""
     argparser = argparse.ArgumentParser(description=__doc__)
-    argparser.add_argument('-i', '--infile', required=True, type=file,
-        help='CSV file containing filament coordinates')
+    argparser.add_argument('-p', '--p3d', required=True, type=file,
+        help='CSV file containing filament coordinates (Point3D)')
     argparser.add_argument('-e', '--edges', required=True, type=file,
         help='CSV file containing filament edges')
     argparser.add_argument('-o', '--outfile', type=argparse.FileType('w'),
@@ -30,9 +30,6 @@ def parse_arguments():
         help='plot parsed filament data')
     argparser.add_argument('--export-plot', dest='export_plot', default=False,
         help='path to export PNG series of plotted filament data')
-    argparser.add_argument('--showmatrix', dest='showmatrix',
-        action='store_const', const=True, default=False,
-        help='show the distance matrix and the longest distance pair')
     argparser.add_argument('-v', '--verbose', dest='verbosity',
         action='count', default=0)
     try:
@@ -46,7 +43,7 @@ def main():
     args = parse_arguments()
     set_loglevel(args.verbosity)
 
-    junction = vp.CellJunction(args.infile, args.edges)
+    junction = vp.CellJunction(args.p3d, args.edges)
 
     if args.outfile:
         junction.write_output(args.outfile, args.infile)
