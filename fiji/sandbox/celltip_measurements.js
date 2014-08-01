fb57fe9cd669057a8206a8775acefa67308626fa
Niko Ehrenfeuchter <mail@he1ix.org>
2014-08-02 01:11:11 +0200

    Use the ChannelSplitter class directly.

--- fiji/sandbox/celltip_measurements.js
+++ fiji/sandbox/celltip_measurements.js
@@ -1,5 +1,5 @@
 /* TODO:
- *  - use ChannelSplitter() etc.?
+ *  - replace IJ.run / rm.runCommand by proper code
  */
 
 importClass(Packages.ij.IJ);
@@ -7,6 +7,7 @@ importClass(Packages.ij.Prefs);
 importClass(Packages.ij.WindowManager);
 importClass(Packages.ij.io.SaveDialog);
 importClass(Packages.ij.plugin.ImageCalculator);
+importClass(Packages.ij.plugin.ChannelSplitter);
 importClass(Packages.ij.plugin.frame.RoiManager);
 importClass(Packages.ij.plugin.filter.ParticleAnalyzer);
 importClass(Packages.ij.measure.ResultsTable);
@@ -17,24 +18,16 @@ imp = IJ.getImage();
 name = imp.title;
 z_slice = imp.z;  // remember the z-position
 
-/*
+
 cs = new ChannelSplitter();
 channels = cs.split(imp);
-//channels[0]
-//imp[C1-140328-AtT20-3-V-ACTH-30-60-30.lsm (1024x1024x1x23x1)]
-//channels[0].show();
-ic = new ImageCalculator();
-combined = ic.run("Add create stack", channels[1], channels[2]);
-*/
-
-IJ.run(imp, "Split Channels", "");
-
-imp_c1 = WindowManager.getImage("C1-" + name);
-imp_c2 = WindowManager.getImage("C2-" + name);
-imp_c3 = WindowManager.getImage("C3-" + name);
+// we need to show the channels to make the "Measure" command work,
+// if we replace it by proper code this is probably not required:
+channels[1].show();
+channels[2].show();
 
 ic = new ImageCalculator();
-imp_combined = ic.run("Add create stack", imp_c2, imp_c3);
+imp_combined = ic.run("Add create stack", channels[1], channels[2]);
 imp_combined.show();
 imp_combined.setZ(z_slice);
 
@@ -68,5 +61,5 @@ function measure_save(imp) {
 	}
 }
 
-measure_save(imp_c2);
-measure_save(imp_c3);
+measure_save(channels[1]);
+measure_save(channels[2]);
