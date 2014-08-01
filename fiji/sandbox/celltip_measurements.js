be45232c775ee692beb9da71aecb85d3421ea509
Niko Ehrenfeuchter <mail@he1ix.org>
2014-08-01 18:42:17 +0200

    Prototype version measuring two channels and saving results.

--- fiji/sandbox/celltip_measurements.js
+++ fiji/sandbox/celltip_measurements.js
@@ -1,13 +1,18 @@
 /* TODO:
- *  - measure both channels
- *  - save results and ROIs into files
+ *  - use proper imp# names
+ *  - use ChannelSplitter() etc.?
  */
 
 importClass(Packages.ij.IJ);
 importClass(Packages.ij.Prefs);
 importClass(Packages.ij.WindowManager);
+importClass(Packages.ij.io.SaveDialog);
 importClass(Packages.ij.plugin.ImageCalculator);
 importClass(Packages.ij.plugin.frame.RoiManager);
+importClass(Packages.ij.plugin.filter.ParticleAnalyzer);
+importClass(Packages.ij.measure.ResultsTable);
+importClass(Packages.ij.measure.Measurements);
+
 
 imp = IJ.getImage();
 name = imp.title;
@@ -26,7 +31,7 @@ combined = ic.run("Add create stack", channels[1], channels[2]);
 IJ.run(imp, "Split Channels", "");
 
 imp = WindowManager.getImage("C1-" + name);
-imp.close();
+//imp.close();
 
 imp1 = WindowManager.getImage("C2-" + name);
 imp2 = WindowManager.getImage("C3-" + name);
@@ -47,9 +52,35 @@ IJ.run(imp3, "Convert to Mask", "method=IJ_IsoData background=Dark black");
 
 IJ.run("Set Measurements...", "area mean min center integrated redirect=None decimal=0");
 IJ.run(imp3, "Analyze Particles...", "size=25-Infinity pixel exclude clear include add slice");
-IJ.selectWindow("C3-" + name);
 
+// prepare the ROI Manager
 rm = RoiManager.getInstance();
 if (rm==null) rm = new RoiManager();
-rm.runCommand("Measure");
 
+// channel 3 measuring and saving
+IJ.selectWindow("C3-" + name);
+rm.runCommand("Measure");
+rt = ResultsTable.getResultsTable();
+rtw = ResultsTable.getResultsWindow();
+rtw.rename('Results ' + imp2.shortTitle);
+
+sd = new SaveDialog('Save measurement results as...', imp2.shortTitle + "_results", ".csv");
+fout = sd.getFileName();
+if (fout != null) {
+	fout = sd.getDirectory() + fout;
+	rt.saveAs(fout);
+}
+
+// channel 2 measuring and saving
+IJ.selectWindow("C2-" + name);
+rm.runCommand("Measure");
+rt = ResultsTable.getResultsTable();
+rtw = ResultsTable.getResultsWindow();
+rtw.rename('Results ' + imp1.shortTitle);
+
+sd = new SaveDialog('Save measurement results as...', imp1.shortTitle + "_results", ".csv");
+fout = sd.getFileName();
+if (fout != null) {
+	fout = sd.getDirectory() + fout;
+	rt.saveAs(fout);
+}
