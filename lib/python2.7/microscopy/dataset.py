d525e3d0e1d36d1f2508f9f8f26cb0b29f971302
Niko Ehrenfeuchter <mail@he1ix.org>
2014-10-29 18:31:59 +0100

    Change ImageDataOIF to inherit from the new ImageDataOlympus superclass.

--- lib/python2.7/microscopy/dataset.py
+++ lib/python2.7/microscopy/dataset.py
@@ -244,7 +244,7 @@ class ImageDataOlympus(ImageData):
         self.position['relative'] = (pos_x, pos_y)
 
 
-class ImageDataOIF(ImageData):
+class ImageDataOIF(ImageDataOlympus):
 
     """Specific DataSet class for images in Olympus OIF format."""
 
@@ -261,39 +261,10 @@ class ImageDataOIF(ImageData):
         For inherited variables, see ImageData.
         """
         log.debug("ImageDataOIF(%s)" % st_path)
-        super(ImageDataOIF, self).__init__('stack', 'tree', st_path)
-        self.storage = self.validate_oifpath()
+        super(ImageDataOIF, self).__init__(st_path)
+        self.storage = self.validate_filepath()
         self.parser = self.setup_parser()
         self._dim = None  # override _dim to mark it as not yet known
-
-    def validate_oifpath(self):
-        """Fix the broken .oif paths in FluoView experiment files.
-
-        The FluoView software usually stores corrupted filenames in its
-        experiment description files, that have a missing suffix, e.g.
-
-            Slide1sec001\\Slide1sec001.oif
-
-        whereas the correct filename would be
-
-            Slide1sec001\\Slide1sec001_01.oif
-
-        This function attempts to fix this by checking if the supplied path is
-        actually existing and trying the default suffix if not. Raises an
-        IOError exception if no corresponding file can be found.
-
-        Returns
-        -------
-        storage : pathtools.parse_path
-        """
-        oif = self.storage
-        log.debug("Validating oif path: %s" % oif)
-        if not exists(oif['full']):
-            oif = parse_path(oif['orig'].replace('.oif', '_01.oif'))
-            log.debug("Trying next path: %s" % oif['full'])
-        if not exists(oif['full']):
-            raise IOError("Can't find OIF file: %s" % oif)
-        return oif
 
     def setup_parser(self):
         """Set up the ConfigParser object for this .oif file.
@@ -313,73 +284,6 @@ class ImageDataOIF(ImageData):
         conv.close()
         log.debug('Finished parsing OIF file.')
         return parser
-
-    def parse_dimensions(self):
-        """Read image dimensions from a ConfigParser object.
-
-        Returns
-        -------
-        dim : (int, int)
-            Pixel dimensions in X and Y direction as tuple.
-        """
-        get = self.parser.get
-        try:
-            dim_b = get(u'Reference Image Parameter', u'ValidBitCounts')
-            dim_x = get(u'Reference Image Parameter', u'ImageHeight')
-            dim_y = get(u'Reference Image Parameter', u'ImageWidth')
-            dim_z = get(u'Axis 3 Parameters Common', u'MaxSize')
-            axis_z = get(u'Axis 3 Parameters Common', u'AxisName')
-            dim_c = get(u'Axis 2 Parameters Common', u'MaxSize')
-            axis_c = get(u'Axis 2 Parameters Common', u'AxisName')
-            dim_t = get(u'Axis 4 Parameters Common', u'MaxSize')
-            axis_t = get(u'Axis 4 Parameters Common', u'AxisName')
-        except ConfigParser.NoOptionError as err:
-            raise ValueError("Error parsing dimensions from %s: %s" %
-                             (self.storage['full'], err))
-        # check if we got the right axis for Z/Ch/T, set to 0 otherwise:
-        if not axis_z == u'"Z"':
-            log.warn("WARNING: couldn't find Z axis in metadata!")
-            dim_z = 0
-        if not axis_c == u'"Ch"':
-            log.warn("WARNING: couldn't find channels in metadata!")
-            dim_c = 0
-        if not axis_t == u'"T"':
-            log.warn("WARNING: couldn't find timepoints in metadata!")
-            dim_t = 0
-        dim = {
-            'B': int(dim_b),  # bit depth
-            'C': int(dim_c),  # channels
-            'T': int(dim_t),  # timepoints
-            'X': int(dim_x),
-            'Y': int(dim_y),
-            'Z': int(dim_z)
-        }
-        log.info('Parsed image dimensions: %s' % dim)
-        return dim
-
-    def get_dimensions(self):
-        """Lazy parsing of the image dimensions."""
-        if self._dim is None:
-            self._dim = self.parse_dimensions()
-        return self._dim
-
-    def set_relpos(self, overlap):
-        """Set the relative coordinates in pixels for this object.
-
-        Parameters
-        ----------
-        overlap : float
-            The overlap between tiles in percent.
-        """
-        ratio = (100.0 - overlap) / 100
-        size_x = self.get_dimensions()['X']
-        size_y = self.get_dimensions()['Y']
-        tileno_x = self.supplement['tileno'][0]
-        tileno_y = self.supplement['tileno'][1]
-        pos_x = size_x * ratio * tileno_x
-        pos_y = size_y * ratio * tileno_y
-        log.info("Setting relative coordinates: %s, %s." % (pos_x, pos_y))
-        self.position['relative'] = (pos_x, pos_y)
 
 
 class ImageDataOIB(ImageDataOlympus):
