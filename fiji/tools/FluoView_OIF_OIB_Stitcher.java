c7f8015c7a1bf23c69eb408c35ce6638eb23bb38
Niko Ehrenfeuchter <mail@he1ix.org>
2014-11-05 14:54:53 +0100

    Reflect renaming *inside* plugin + wrapper.

--- fiji/tools/FluoView_OIF_OIB_Stitcher.java
+++ fiji/tools/FluoView_OIF_OIB_Stitcher.java
@@ -2,11 +2,11 @@ import ij.IJ;
 
 import ij.plugin.PlugIn;
 
-public class FluoView_OIF_Stitcher implements PlugIn {
+public class FluoView_OIF_OIB_Stitcher implements PlugIn {
 	public void run(String arg) {
 		try {
 			new Jython.Refresh_Jython_Scripts().runScript(getClass()
-				.getResource("FluoView_OIF_Stitcher.py").openStream());
+				.getResource("FluoView_OIF_OIB_Stitcher.py").openStream());
 		} catch (Exception e) {
 			IJ.handleException(e);
 		}
