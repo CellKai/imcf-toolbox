3a5eab487a5de76aa6dcb2e9d479739078f398ff
Niko Ehrenfeuchter <mail@he1ix.org>
2014-11-08 17:32:58 +0100

    Python script to apply a Weka classifier to all TIFFs in a directory.

---
+++ fiji/sandbox/WEKA_run_classifier_on_directory.py
@@ -0,0 +1,31 @@
+import os
+from timeit import default_timer as timer
+from trainableSegmentation import WekaSegmentation
+from ij import IJ
+
+indir = "/scratch/data/__TESTFILES/weka"
+modelfile = os.path.join(indir, "bg_vs_tissue.model")
+infile = os.path.join(indir, "1462_mko_ctx_1.tif")
+
+input_image = IJ.openImage(infile)
+
+segmentator = WekaSegmentation(input_image)
+# segmentator = WekaSegmentation()
+IJ.log("Loading classifier...")
+t_start = timer()
+segmentator.loadClassifier(modelfile)
+t_end = timer()
+IJ.log("Loading classifier completed (%.2fs)." % (t_end - t_start))
+
+for root, directories, filenames in os.walk(indir):
+    for filename in filenames:
+      # Skip non-TIFF files
+      if not filename.endswith(".tif"):
+        continue
+      path = os.path.join(root, filename)
+      IJ.log("Classifying %s..." % path)
+      input_image = IJ.openImage(path)
+      result = segmentator.applyClassifier(input_image, 0, True)
+      IJ.log("Finished classification, saving results...")
+      IJ.save(result, path + "_result.tif")
+      IJ.log("Saved results as '%s'." % path + "_result.tif")
