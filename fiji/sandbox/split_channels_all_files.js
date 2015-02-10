7da5be17c6acdade7f232414d0b9227aab29fc46
Niko Ehrenfeuchter <mail@he1ix.org>
2015-02-10 11:40:00 +0100

    Prototype to split all files into subdirs by channels.

---
+++ fiji/sandbox/split_channels_all_files.js
@@ -0,0 +1,23 @@
+importClass(Packages.ij.IJ);
+importClass(Packages.ij.WindowManager);
+importClass(Packages.java.io.File);
+
+var dir = '/home/ehrenfeu/imageproc/data/fabia_morgane/BI19/';
+files = File(dir).list();
+
+for (var i = 0; i < files.length; i++) {
+    print(files[i]);
+    imp = IJ.openImage(dir + files[i]);
+	imp.show();
+	IJ.run(imp,  "Split Channels", "");
+	ids = WindowManager.getIDList();
+	print(ids.length);
+	for (var j = 0; j < ids.length; j++) {
+		impnew = WindowManager.getImage(ids[j]);
+		channel = impnew.getTitle().split("-")[0];
+		print(channel);
+		File(dir + channel).mkdir();
+	}
+
+	break;
+}
