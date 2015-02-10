929c23cf628b6265113971b3fdf5687a886030df
Niko Ehrenfeuchter <mail@he1ix.org>
2015-02-10 12:59:32 +0100

    Use the ChannelSplitter() class instead of IJ.run().

--- fiji/sandbox/split_channels_all_files.js
+++ fiji/sandbox/split_channels_all_files.js
@@ -1,5 +1,5 @@
 importClass(Packages.ij.IJ);
-importClass(Packages.ij.WindowManager);
+importClass(Packages.ij.plugin.ChannelSplitter);
 importClass(Packages.ij.io.DirectoryChooser);
 importClass(Packages.ij.io.FileSaver);
 importClass(Packages.java.io.File);
@@ -9,20 +9,17 @@ print(dir);
 
 files = File(dir).list();
 for (var i = 0; i < files.length; i++) {
-    print(files[i]);
+    print("Processing file " + files[i]);
     imp = IJ.openImage(dir + files[i]);
-	imp.show();
-	IJ.run(imp,  "Split Channels", "");
-	ids = WindowManager.getIDList();
-	print(ids.length);
-   title = imp.getTitle();
-	for (var j = 0; j < ids.length; j++) {
-		impnew = WindowManager.getImage(ids[j]);
-		channel = impnew.getTitle().split("-")[0];
-		print(channel);
-		File(dir + channel).mkdir();
-		FileSaver(impnew.saveAsTiffStack(dir + ch_name + "/" + title);
+    title = imp.getTitle();
+	//imp.show();
+	channels = ChannelSplitter().split(imp);
+	for (var j = 0; j < channels.length; j++) {
+		ch_name = channels[j].getTitle().split("-")[0];
+		//print(ch_name);
+		File(dir + ch_name).mkdir();
+		print("Writing channel " + ch_name);
+		FileSaver(channels[j]).saveAsTiffStack(dir + ch_name + "/" + title);
 	}
-
 	break;
 }
