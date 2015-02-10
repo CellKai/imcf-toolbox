3614d889b5b96d36d96287653f13e6ce45a64f11
Niko Ehrenfeuchter <mail@he1ix.org>
2015-02-10 12:57:26 +0100

    Save channels to individual TIFF stacks.

--- fiji/sandbox/split_channels_all_files.js
+++ fiji/sandbox/split_channels_all_files.js
@@ -1,5 +1,6 @@
 importClass(Packages.ij.IJ);
 importClass(Packages.ij.WindowManager);
+importClass(Packages.ij.io.FileSaver);
 importClass(Packages.java.io.File);
 
 var dir = '/home/ehrenfeu/imageproc/data/fabia_morgane/BI19/';
@@ -12,11 +13,13 @@ for (var i = 0; i < files.length; i++) {
 	IJ.run(imp,  "Split Channels", "");
 	ids = WindowManager.getIDList();
 	print(ids.length);
+   title = imp.getTitle();
 	for (var j = 0; j < ids.length; j++) {
 		impnew = WindowManager.getImage(ids[j]);
 		channel = impnew.getTitle().split("-")[0];
 		print(channel);
 		File(dir + channel).mkdir();
+		FileSaver(impnew.saveAsTiffStack(dir + ch_name + "/" + title);
 	}
 
 	break;
