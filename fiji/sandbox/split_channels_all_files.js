26e8430d97cd9602c87bd35f4bbec2b5ea2d31f2
Niko Ehrenfeuchter <mail@he1ix.org>
2015-02-10 19:44:44 +0100

    Split Z slices into separate files.

--- fiji/sandbox/split_channels_all_files.js
+++ fiji/sandbox/split_channels_all_files.js
@@ -1,4 +1,6 @@
 importClass(Packages.ij.IJ);
+importClass(Packages.ij.ImagePlus);
+importClass(Packages.ij.process.ImageProcessor);
 importClass(Packages.ij.plugin.ChannelSplitter);
 importClass(Packages.ij.io.DirectoryChooser);
 importClass(Packages.ij.io.FileSaver);
@@ -12,14 +14,24 @@ for (var i = 0; i < files.length; i++) {
     print("Processing file " + files[i]);
     imp = IJ.openImage(dir + files[i]);
     title = imp.getTitle();
-	//imp.show();
+    splitpos = title.lastIndexOf(".");
+    if (splitpos === -1) {
+		t_name = title;
+		t_suff = "";
+    } else {
+		t_name = title.substr(0, splitpos);
+		t_suff = title.substr(splitpos, title.length());
+    }
 	channels = ChannelSplitter().split(imp);
 	for (var j = 0; j < channels.length; j++) {
 		ch_name = channels[j].getTitle().split("-")[0];
-		//print(ch_name);
 		File(dir + ch_name).mkdir();
-		print("Writing channel " + ch_name);
-		FileSaver(channels[j]).saveAsTiffStack(dir + ch_name + "/" + title);
+		stack = channels[j].getStack();
+		for (var z=1; z<=stack.getSize(); z++) {
+			ip = stack.getProcessor(z);
+			fname = ch_name + "/" + t_name + "-z" + z + t_suff;
+			print("Writing channel " + ch_name + ", slice " + z + ": " + fname);
+			FileSaver(ImagePlus(title, ip)).saveAsTiff(dir + fname);
+		}
 	}
-	break;
 }
