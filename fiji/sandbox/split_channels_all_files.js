cfc58858f5a197b81e33e568461c15c0b61236a4
Niko Ehrenfeuchter <mail@he1ix.org>
2015-02-10 22:30:31 +0100

    Add dialog to specify number of slices to skip.

--- fiji/sandbox/split_channels_all_files.js
+++ fiji/sandbox/split_channels_all_files.js
@@ -1,5 +1,6 @@
 importClass(Packages.ij.IJ);
 importClass(Packages.ij.ImagePlus);
+importClass(Packages.ij.gui.GenericDialog);
 importClass(Packages.ij.io.DirectoryChooser);
 importClass(Packages.ij.io.FileSaver);
 importClass(Packages.ij.process.ImageProcessor);
@@ -8,6 +9,14 @@ importClass(Packages.java.io.File);
 
 var dir = DirectoryChooser("Select a directory...").getDirectory();
 print(dir);
+gd = new GenericDialog("Skip slices");
+gd.addNumericField("Skip slices at top", 0, 0);
+gd.addNumericField("Skip slices at bottom", 0, 0);
+gd.showDialog();
+skip_t = gd.getNextNumber();
+skip_b = gd.getNextNumber();
+//IJ.log(skip_t + " " + skip_b);
+//print(foo);
 
 files = File(dir).list();
 for (var i = 0; i < files.length; i++) {
@@ -27,7 +36,9 @@ for (var i = 0; i < files.length; i++) {
 		ch_name = channels[j].getTitle().split("-")[0];
 		File(dir + ch_name).mkdir();
 		stack = channels[j].getStack();
-		for (var z=1; z<=stack.getSize(); z++) {
+		z_start = 1 + skip_t;
+		z_stop  = stack.getSize() - skip_b;
+		for (var z=z_start; z<=z_stop; z++) {
 			ip = stack.getProcessor(z);
 			fname = ch_name + "/" + t_name + "-z" + z + t_suff;
 			print("Writing channel " + ch_name + ", slice " + z + ": " + fname);
