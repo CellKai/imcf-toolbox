bb7fdff97ec91dc4d260b1b8a779aa2dfe3f2215
Niko Ehrenfeuchter <mail@he1ix.org>
2015-02-10 22:55:52 +0100

    Put filename processing into a function.

--- fiji/sandbox/split_channels_all_files.js
+++ fiji/sandbox/split_channels_all_files.js
@@ -18,6 +18,19 @@ function getSlicesToSkip() {
 	return skip;
 }
 
+function splitFileName(fname) {
+	var pieces = [];
+	var splitpos = fname.lastIndexOf(".");
+    if (splitpos === -1) {
+		pieces[0] = fname;
+		pieces[1] = "";
+    } else {
+		pieces[0] = fname.substr(0, splitpos);
+		pieces[1] = fname.substr(splitpos);
+    }
+	return pieces;
+}
+
 dir = DirectoryChooser("Select a directory...").getDirectory();
 skip = getSlicesToSkip();
 
@@ -25,25 +38,17 @@ files = File(dir).list();
 for (var i = 0; i < files.length; i++) {
     print("Processing file " + files[i]);
     imp = IJ.openImage(dir + files[i]);
-    title = imp.getTitle();
-    splitpos = title.lastIndexOf(".");
-    if (splitpos === -1) {
-		t_name = title;
-		t_suff = "";
-    } else {
-		t_name = title.substr(0, splitpos);
-		t_suff = title.substr(splitpos, title.length());
-    }
-	channels = ChannelSplitter().split(imp);
+    fname = splitFileName(files[i]);
+    channels = ChannelSplitter().split(imp);
 	for (var j = 0; j < channels.length; j++) {
 		ch_name = channels[j].getTitle().split("-")[0];
 		File(dir + ch_name).mkdir();
 		stack = channels[j].getStack();
 		for (var z = 1 + skip[0] ; z <= stack.getSize() - skip[1] ; z++) {
 			ip = stack.getProcessor(z);
-			fname = ch_name + "/" + t_name + "-z" + z + t_suff;
-			print("Writing channel " + ch_name + ", slice " + z + ": " + fname);
-			FileSaver(ImagePlus(title, ip)).saveAsTiff(dir + fname);
+			fout = ch_name + "/" + fname[0] + "-z" + z + fname[1];
+			print("Writing channel " + ch_name + ", slice " + z + ": " + fout);
+			FileSaver(ImagePlus(fname[0], ip)).saveAsTiff(dir + fout);
 		}
 	}
 }
