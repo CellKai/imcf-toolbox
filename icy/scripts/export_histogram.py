c26f26ae32393bbc0d2d2d94471a936ed7f561f6
Niko Ehrenfeuchter <mail@he1ix.org>
2015-04-09 18:46:47 +0200

    Fix indenting (spaces instead of tabs).

--- icy/scripts/export_histogram.py
+++ icy/scripts/export_histogram.py
@@ -7,28 +7,28 @@ XLS_FILE = '/tmp/icy_py_excel_test.xls'
 
 
 def get_histogram(seq, nbins, bin_min, bin_max):
-	"""Generate a list of [val, count] from the histogram."""
-	hist = Histogram.compute(seq, nbins, bin_min, bin_max)
-	hist_list = []
-	for i in xrange(hist.getNbBins()):
-		bin = hist.getBin(i)
-		val = bin.getCentralValue()
-		count = bin.getCount()
-		hist_list.append([val, count])
-	return hist_list
+    """Generate a list of [val, count] from the histogram."""
+    hist = Histogram.compute(seq, nbins, bin_min, bin_max)
+    hist_list = []
+    for i in xrange(hist.getNbBins()):
+        bin = hist.getBin(i)
+        val = bin.getCentralValue()
+        count = bin.getCount()
+        hist_list.append([val, count])
+    return hist_list
 
 
 def new_xls_row(ws, values):
-	"""Add a new row from a list of values to a worksheet."""
-	global row
-	col = 0
-	for val in values:
-		if type(val) is str or type(val) is unicode:
-			XLSUtil.setCellString(ws, col, row, val)
-		else:
-			XLSUtil.setCellNumber(ws, col, row, val)
-		col += 1
-	row += 1
+    """Add a new row from a list of values to a worksheet."""
+    global row
+    col = 0
+    for val in values:
+        if type(val) is str or type(val) is unicode:
+            XLSUtil.setCellString(ws, col, row, val)
+        else:
+            XLSUtil.setCellNumber(ws, col, row, val)
+        col += 1
+    row += 1
 
 
 seq = Icy.getMainInterface().getFocusedSequence()
@@ -64,15 +64,15 @@ new_xls_row(ws, ['Bin width', bwh*2])
 row += 2
 
 for c in xrange(num_c):
-	channel = seq.extractChannel(c)
-	hist = get_histogram(channel, NUM_BINS, val_min + bwh, val_max - bwh)
-	new_xls_row(ws, ['Histogram for channel', c])
-	overall = 0
-	for i in hist:
-		new_xls_row(ws, [i[0], i[1]])
-		overall += i[1]
-	new_xls_row(ws, ['Overall count from bins', overall])
-	row += 2
+    channel = seq.extractChannel(c)
+    hist = get_histogram(channel, NUM_BINS, val_min + bwh, val_max - bwh)
+    new_xls_row(ws, ['Histogram for channel', c])
+    overall = 0
+    for i in hist:
+        new_xls_row(ws, [i[0], i[1]])
+        overall += i[1]
+    new_xls_row(ws, ['Overall count from bins', overall])
+    row += 2
 
 # close and save the excel file
 XLSUtil.saveAndClose(wb)
